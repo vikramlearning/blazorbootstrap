68f31c34ab75cbe948010fdc8ba6a1c4c2878b5c
Vikram Reddy <gvreddy04@gmail.com>
2021-11-19 17:18:48 +0530

    Offcanvas updated.

--- blazorbootstrap/Components/Offcanvas/Offcanvas.razor.cs
+++ blazorbootstrap/Components/Offcanvas/Offcanvas.razor.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace BlazorBootstrap
 {
-    public partial class Offcanvas : IObserver<ElementReference>, IDisposable
+    public partial class Offcanvas : IDisposable
     {
         [Inject] OffcanvasService OffcanvasService { get; set; }
         [Inject] IJSRuntime JS { get; set; }
@@ -13,14 +13,31 @@ namespace BlazorBootstrap
         public ElementReference ElementRef { get; set; }
         [Parameter] public string ElementId { get; set; }
         [Parameter] public Placement Placement { get; set; } = Placement.Right;
-        [Parameter] public EventCallback OnOffcanvasShow { get; set; }
-        [Parameter] public EventCallback OnOffcanvasShown { get; set; }
-        [Parameter] public EventCallback OnOffcanvasHide { get; set; }
-        [Parameter] public EventCallback OnOffcanvasHidden { get; set; }
+
+        /// <summary>
+        /// This event fires immediately when the show instance method is called.
+        /// </summary>
+        [Parameter] public EventCallback Showing { get; set; }
+
+        /// <summary>
+        /// This event is fired when an offcanvas element has been made visible to the user (will wait for CSS transitions to complete).
+        /// </summary>
+        [Parameter] public EventCallback Shown { get; set; }
+
+        /// <summary>
+        /// This event is fired immediately when the hide method has been called.
+        /// </summary>
+        [Parameter] public EventCallback Hiding { get; set; }
+
+        /// <summary>
+        /// This event is fired when an offcanvas element has been hidden from the user (will wait for CSS transitions to complete).
+        /// </summary>
+        [Parameter] public EventCallback Hidden { get; set; }
 
         private DotNetObjectReference<Offcanvas> objRef;
 
         private string offcanvasPlacement = "offcanvas-end"; // default
+        private bool isShown = false; // default
 
         protected override void OnInitialized()
         {
@@ -32,6 +49,8 @@ namespace BlazorBootstrap
 
         private void Show(string elementId)
         {
+            isShown = true;
+
             offcanvasPlacement = Placement switch
             {
                 Placement.Right => "offcanvas-end",
@@ -42,37 +61,26 @@ namespace BlazorBootstrap
 
             Task.Run(async () =>
             {
-                objRef = objRef ?? DotNetObjectReference.Create(this);
+                objRef ??= DotNetObjectReference.Create(this);
                 await JS.InvokeVoidAsync("blazorBootstrap.offcanvas.show", elementId, objRef);
             });
         }
 
         private void Hide(string elementId)
         {
-            Task.Run(async () => { await JS.InvokeVoidAsync("blazorBootstrap.offcanvas.hide", elementId); });
+            Task.Run(async () =>
+            {
+                await JS.InvokeVoidAsync("blazorBootstrap.offcanvas.hide", elementId);
+                //isShown = false;
+            });
         }
 
-        [JSInvokable] public async Task bsShowOffcanvas() => await OnOffcanvasShow.InvokeAsync();
-        [JSInvokable] public async Task bsShownOffcanvas() => await OnOffcanvasShown.InvokeAsync();
-        [JSInvokable] public async Task bsHideOffcanvas() => await OnOffcanvasHide.InvokeAsync();
-        [JSInvokable] public async Task bsHiddenOffcanvas() => await OnOffcanvasHidden.InvokeAsync();
+        [JSInvokable] public async Task bsShowOffcanvas() => await Showing.InvokeAsync();
+        [JSInvokable] public async Task bsShownOffcanvas() => await Shown.InvokeAsync();
+        [JSInvokable] public async Task bsHideOffcanvas() => await Hiding.InvokeAsync();
+        [JSInvokable] public async Task bsHiddenOffcanvas() => await Hidden.InvokeAsync();
 
         #region Implementations
-
-        public void OnCompleted()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void OnError(Exception error)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void OnNext(ElementReference value)
-        {
-            throw new NotImplementedException();
-        }
 
         public void Dispose()
         {
