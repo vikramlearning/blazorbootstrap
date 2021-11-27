03ba4cb205f6c8931659af0bbada2fcb7182ae0f
Vikram Reddy <gvreddy04@gmail.com>
2021-11-27 13:46:12 +0530

    Feature/footer update (#3)
    * Copyright updated.

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -106,7 +106,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Blazor Bootstrap.`,
       },
       prism: {
         theme: lightCodeTheme,
