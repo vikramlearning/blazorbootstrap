85ea2ea171efc4dbe93bbb65b2b211cf952d39b6
Vijay Reddy Gaddam <gvreddy579@users.noreply.github.com>
2021-11-27 13:12:01 +0530

    [Documentation] Added documentation portal using  Docusaurus (#1)
    * Added Documentation setup and github actions
    * Changing trigger to push on master branch only

---
+++ docs/docusaurus.config.js
@@ -0,0 +1,118 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'blazorbootstrap',
+  tagline: 'Blazer with bootstrap is cool',
+  url: 'https://gvreddy04.github.io',
+  baseUrl: '/blazorbootstrap/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'gvreddy04',
+  projectName: 'blazorbootstrap',
+  trailingSlash: false,
+  deploymentBranch: 'gh-pages',
+
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl: 'https://github.com/gvreddy04/blazorbootstrap/edit/masters/docs/docs',
+        },
+        blog: {
+          showReadingTime: true,
+          editUrl:
+            'https://github.com/gvreddy04/blazorbootstrap/edit/master/docs/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'My Site',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Tutorial',
+          },
+          {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/gvreddy04/blazorbootstrap',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/blazorbootstrap',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/blazorbootstrap',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/blazorbootstrap',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/gvreddy04/blazorbootstrap',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
