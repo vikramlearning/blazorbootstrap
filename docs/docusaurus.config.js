6b332f6969ab2429331814c1ebb4afe4dee7e7f6
Vikram Reddy <gvreddy04@gmail.com>
2021-12-10 13:06:21 +0530

    18 update existing components docs (#20)
    * cryptodevhub.io - theme applied and landing page emojis update.

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,6 +1,3 @@
-// @ts-check
-// Note: type annotations allow type checking and IDEs autocompletion
-
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
@@ -18,11 +15,60 @@ const config = {
     trailingSlash: false,
     deploymentBranch: 'gh-pages',
 
+    themeConfig: {
+        navbar: {
+            title: 'BlazorBootstrap',
+            logo: {
+                alt: 'BlazorBootstrap',
+                src: 'img/logo.svg',
+            },
+            items: [
+                { type: 'doc', docId: 'intro', label: 'Doc', position: 'left' },
+                //{ to: '/blog', label: 'Blog', position: 'left' },
+                { href: 'https://github.com/vikramlearning/blazorbootstrap', label: 'GitHub', position: 'right', },
+            ],
+        },
+        footer: {
+            style: 'dark',
+            links: [
+                {
+                    title: 'Docs',
+                    items: [
+                        { label: 'Introduction', to: '/docs/intro', },
+                    ],
+                },
+                {
+                    title: 'Community',
+                    items: [
+                        { label: 'Stack Overflow', href: 'https://stackoverflow.com/questions/tagged/blazorbootstrap', },
+                        { label: 'Twitter', href: 'https://twitter.com/blazorbootstrap', },
+                    ],
+                },
+                {
+                    title: 'More',
+                    items: [
+                        //{ label: 'Blog', to: '/blog', },
+                        { label: 'GitHub', href: 'https://github.com/vikramlearning/blazorbootstrap', },
+                    ],
+                },
+            ],
+            copyright: `Copyright © ${new Date().getFullYear()} Vikram Learning.`,
+        },
+        prism: {
+            theme: lightCodeTheme,
+            darkTheme: darkCodeTheme,
+            additionalLanguages: ['csharp', 'cshtml'],
+        },
+        googleAnalytics: {
+            trackingID: 'UA-214301343-1',
+            // Optional fields.
+            anonymizeIP: true, // Should IPs be anonymized?
+        },
+    },
     presets: [
         [
             '@docusaurus/preset-classic',
-            /** @type {import('@docusaurus/preset-classic').Options} */
-            ({
+            {
                 docs: {
                     sidebarPath: require.resolve('./sidebars.js'),
                     editUrl: 'https://github.com/vikramlearning/blazorbootstrap/edit/master/docs/',
@@ -34,90 +80,21 @@ const config = {
                 theme: {
                     customCss: require.resolve('./src/css/custom.css'),
                 },
-            }),
+            },
         ],
     ],
-
-    themeConfig:
-        /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-        ({
-            navbar: {
-                title: 'BlazorBootstrap',
-                logo: {
-                    alt: 'BlazorBootstrap',
-                    src: 'img/logo.svg',
-                },
-                items: [
-                    {
-                        type: 'doc',
-                        docId: 'intro',
-                        position: 'left',
-                        label: 'Docs',
-                    },
-                    //{
-                    //    to: '/blog',
-                    //    position: 'left',
-                    //    label: 'Blog'
-                    //},
-                    {
-                        href: 'https://github.com/vikramlearning/blazorbootstrap',
-                        position: 'right',
-                        label: 'GitHub',
-                    },
-                ],
-            },
-            footer: {
-                style: 'dark',
-                links: [
-                    {
-                        title: 'Docs',
-                        items: [
-                            {
-                                label: 'Introduction',
-                                to: '/docs/intro',
-                            },
-                        ],
-                    },
-                    {
-                        title: 'Community',
-                        items: [
-                            {
-                                label: 'Stack Overflow',
-                                href: 'https://stackoverflow.com/questions/tagged/blazorbootstrap',
-                            },
-                            {
-                                label: 'Twitter',
-                                href: 'https://twitter.com/blazorbootstrap',
-                            },
-                        ],
-                    },
-                    {
-                        title: 'More',
-                        items: [
-                            //{
-                            //    label: 'Blog',
-                            //    to: '/blog',
-                            //},
-                            {
-                                label: 'GitHub',
-                                href: 'https://github.com/vikramlearning/blazorbootstrap',
-                            },
-                        ],
-                    },
-                ],
-                copyright: `Copyright © ${new Date().getFullYear()} Vikram Learning.`,
-            },
-            prism: {
-                theme: lightCodeTheme,
-                darkTheme: darkCodeTheme,
-                additionalLanguages: ['csharp', 'cshtml'],
-            },
-            googleAnalytics: {
-                trackingID: 'UA-214301343-1',
-                // Optional fields.
-                anonymizeIP: true, // Should IPs be anonymized?
-            },
-        }),
+    plugins: [
+        [
+            require.resolve('@easyops-cn/docusaurus-search-local'),
+            {
+                indexDocs: true,
+                indexPages: true,
+                indexBlog: false,
+                docsDir: 'docs',
+                docsRouteBasePath: '/docs'
+            }
+        ]
+    ],
     customFields: {
         version: '0.0.2'
     }
--- docs/src/components/HomepageFeatures.js
+++ docs/src/components/HomepageFeatures.js
@@ -3,59 +3,59 @@ import clsx from 'clsx';
 import styles from './HomepageFeatures.module.css';
 
 const FeatureList = [
-  {
-    title: 'Easy to Use',
-    Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
-      <>
-        BlazorBootstrap has been built with Blazor and Bootstrap CSS framework to use Bootstrap components with ease.
-      </>
-    ),
-  },
-  {
-    title: 'Focus on What Matters',
-    Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        BlazorBootstrap lets you focus on your deliverables, and we'll build the Bootstrap components in Blazor.
-      </>
-    ),
-  },
-  {
-    title: 'Open-source & free',
-    Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        BlazorBootstrap is licensed under the Apache License 2.0. Clone it, fork it, and customize it.
-      </>
-    ),
-  },
+    {
+        title: 'Easy to Use',
+        emoji: ':+1:',
+        description: (
+            <>
+                BlazorBootstrap has been built with Blazor and Bootstrap CSS framework to use Bootstrap components with ease.
+            </>
+        ),
+    },
+    {
+        title: 'Focus on What Matters',
+        emoji: ':monocle_face:',
+        description: (
+            <>
+                BlazorBootstrap lets you focus on your deliverables, and we'll build the Bootstrap components in Blazor.
+            </>
+        ),
+    },
+    {
+        title: 'Open-source & free',
+        emoji: ':unlock:',
+        description: (
+            <>
+                BlazorBootstrap is licensed under the Apache License 2.0. Clone it, fork it, and customize it.
+            </>
+        ),
+    },
 ];
 
-function Feature({Svg, title, description}) {
-  return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} alt={title} />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
+function Feature({ Svg, title, description }) {
+    return (
+        <div className={clsx('col col--4')}>
+            <div className="text--center">
+                <Svg className={styles.featureSvg} alt={title} />
+            </div>
+            <div className="text--center padding-horiz--md">
+                <h3>{title}</h3>
+                <p>{description}</p>
+            </div>
+        </div>
+    );
 }
 
 export default function HomepageFeatures() {
-  return (
-    <section className={styles.features}>
-      <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </div>
-      </div>
-    </section>
-  );
+    return (
+        <section className={styles.features}>
+            <div className="container">
+                <div className="row">
+                    {FeatureList.map((props, idx) => (
+                        <Feature key={idx} {...props} />
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
 }
