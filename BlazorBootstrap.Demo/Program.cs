687eee2f2122abf0b2ed06ec11d6cb0d4eb77a05
Vikram Reddy <gvreddy04@gmail.com>
2021-12-18 20:04:05 +0530

    .NET 6 upgrade and UI project renamed to Demo

---
+++ BlazorBootstrap.Demo/Program.cs
@@ -0,0 +1,15 @@
+using BlazorBootstrap;
+using BlazorBootstrap.Demo;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// Inject services
+builder.Services.AddBlazorBootstrap();
+
+await builder.Build().RunAsync();
