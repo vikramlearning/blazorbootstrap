c7629ffd2e6ebb10767db27b7c16a09bbcab4c41
Vikram Reddy <gvreddy04@hotmail.com>
2022-11-06 01:02:28 +0530

    #62 - generic type support - draft

--- blazorbootstrap/Components/Form/NumberInput.razor.cs
+++ blazorbootstrap/Components/Form/NumberInput.razor.cs
@@ -1,15 +1,16 @@
 ﻿using Microsoft.AspNetCore.Components.Forms;
+using System.Runtime.Serialization;
 
 namespace BlazorBootstrap;
 
-public partial class NumberInput : BaseComponent
+public partial class NumberInput<TValue> : BaseComponent
 {
     #region Events
 
     /// <summary>
     /// This is event fires on every user keystroke that changes the textbox value.
     /// </summary>
-    [Parameter] public EventCallback<int?> ValueChanged { get; set; }
+    [Parameter] public EventCallback<TValue> ValueChanged { get; set; }
 
     #endregion
 
@@ -38,6 +39,26 @@ public partial class NumberInput : BaseComponent
 
     protected override async Task OnInitializedAsync()
     {
+        if (IsLeftGreaterThanRight(Min, Max))
+            throw new InvalidOperationException("The Min parameter value is greater than the Max parameter value.");
+
+        if (!(typeof(TValue) == typeof(sbyte)
+            || typeof(TValue) == typeof(sbyte?)
+            || typeof(TValue) == typeof(short)
+            || typeof(TValue) == typeof(short?)
+            || typeof(TValue) == typeof(int)
+            || typeof(TValue) == typeof(int?)
+            || typeof(TValue) == typeof(long)
+            || typeof(TValue) == typeof(long?)
+            || typeof(TValue) == typeof(float)
+            || typeof(TValue) == typeof(float?)
+            || typeof(TValue) == typeof(double)
+            || typeof(TValue) == typeof(double?)
+            || typeof(TValue) == typeof(decimal)
+            || typeof(TValue) == typeof(decimal?)
+            ))
+            throw new InvalidOperationException($"{typeof(TValue)} is not supported.");
+
         Attributes ??= new Dictionary<string, object>();
 
         fieldIdentifier = FieldIdentifier.Create(ValueExpression);
@@ -53,18 +74,19 @@ public partial class NumberInput : BaseComponent
     {
         await base.OnAfterRenderAsync(firstRender);
 
-        if (!firstRender || Value is null || !(Min.HasValue && Max.HasValue))
+        if (!firstRender || Value is null || !(Min is not null && Max is not null))
             return;
 
         var currentValue = Value; // object
 
-        if (string.IsNullOrWhiteSpace(currentValue?.ToString())
-             || !int.TryParse(currentValue.ToString(), out int value))
-            Value = null;
-        else if (Min.HasValue && value < Min.Value)
-            Value = Min.Value;
-        else if (Max.HasValue && value > Max.Value)
-            Value = Max.Value;
+        if (currentValue is null || !TryParseValue(currentValue, out TValue value))
+            Value = default;
+        else if (IsLeftGreaterThanRight(Min, Value)) // value < min
+            Value = Min;
+        else if (IsLeftGreaterThanRight(Value, Max)) // value > max
+            Value = Max;
+        else
+            Value = value;
 
         await ValueChanged.InvokeAsync(Value);
 
@@ -92,17 +114,12 @@ public partial class NumberInput : BaseComponent
         var oldValue = Value;
         var newValue = e.Value; // object
 
-        if (string.IsNullOrWhiteSpace(newValue?.ToString())
-             || !int.TryParse(newValue.ToString(), out int value))
-            Value = null;
-        //else if (Min.HasValue && value < Min.Value)
-        //    Value = Min.Value;
-        //else if (Max.HasValue && value > Max.Value)
-        //    Value = Max.Value;
+        if (newValue is null || !TryParseValue(newValue, out TValue value))
+            Value = default;
         else
             Value = value;
 
-        if (oldValue == Value)
+        if (oldValue.Equals(Value))
         {
             await JS.InvokeVoidAsync("window.blazorBootstrap.numberInput.setValue", ElementId, Value);
         }
@@ -119,17 +136,16 @@ public partial class NumberInput : BaseComponent
         var oldValue = Value;
         var newValue = e.Value; // object
 
-        if (string.IsNullOrWhiteSpace(newValue?.ToString())
-             || !int.TryParse(newValue.ToString(), out int value))
-            Value = null;
-        else if (Min.HasValue && value < Min.Value)
-            Value = Min.Value;
-        else if (Max.HasValue && value > Max.Value)
-            Value = Max.Value;
+        if (newValue is null || !TryParseValue(newValue, out TValue value))
+            Value = default;
+        else if (IsLeftGreaterThanRight(Min, Value)) // value < min
+            Value = Min;
+        else if (IsLeftGreaterThanRight(Value, Max)) // value > max
+            Value = Max;
         else
             Value = value;
 
-        if (oldValue == Value)
+        if (oldValue.Equals(Value))
         {
             await JS.InvokeVoidAsync("window.blazorBootstrap.numberInput.setValue", ElementId, Value);
         }
@@ -139,6 +155,174 @@ public partial class NumberInput : BaseComponent
         EditContext?.NotifyFieldChanged(fieldIdentifier);
 
         Console.WriteLine($"OnChange - Input: {e.Value?.ToString()}, Value: {Value}"); // TODO: remove this console log
+    }
+
+    /// <summary>
+    /// Determines where the left input is greater than the right input.
+    /// </summary>
+    /// <param name="left"></param>
+    /// <param name="right"></param>
+    /// <returns>bool</returns>
+    private bool IsLeftGreaterThanRight(TValue left, TValue right)
+    {
+        // sbyte
+        if (typeof(TValue) == typeof(sbyte))
+        {
+            sbyte l = Convert.ToSByte(left);
+            sbyte r = Convert.ToSByte(right);
+            return l > r;
+        }
+        // sbyte?
+        else if (typeof(TValue) == typeof(sbyte?))
+        {
+            sbyte? l = left as sbyte?;
+            sbyte? r = right as sbyte?;
+            return l.HasValue && r.HasValue && l > r;
+        }
+        // short / int16
+        else if (typeof(TValue) == typeof(short))
+        {
+            short l = Convert.ToInt16(left);
+            short r = Convert.ToInt16(right);
+            return l > r;
+        }
+        // short? / int16?
+        else if (typeof(TValue) == typeof(short?))
+        {
+            short? l = left as short?;
+            short? r = right as short?;
+            return l.HasValue && r.HasValue && l > r;
+        }
+        // int
+        else if (typeof(TValue) == typeof(int))
+        {
+            int l = Convert.ToInt32(left);
+            int r = Convert.ToInt32(right);
+            return l > r;
+        }
+        // int?
+        else if (typeof(TValue) == typeof(int?))
+        {
+            int? l = left as int?;
+            int? r = right as int?;
+            return l.HasValue && r.HasValue && l > r;
+        }
+        // long
+        else if (typeof(TValue) == typeof(long))
+        {
+            long l = Convert.ToInt64(left);
+            long r = Convert.ToInt64(right);
+            return l > r;
+        }
+        // long?
+        else if (typeof(TValue) == typeof(long?))
+        {
+            long? l = left as long?;
+            long? r = right as long?;
+            return l.HasValue && r.HasValue && l > r;
+        }
+        // float / single
+        else if (typeof(TValue) == typeof(float))
+        {
+            float l = Convert.ToSingle(left);
+            float r = Convert.ToSingle(right);
+            return l > r;
+        }
+        // float? / single?
+        else if (typeof(TValue) == typeof(float?))
+        {
+            float? l = left as float?;
+            float? r = right as float?;
+            return l.HasValue && r.HasValue && l > r;
+        }
+        // double
+        else if (typeof(TValue) == typeof(double))
+        {
+            double l = Convert.ToDouble(left);
+            double r = Convert.ToDouble(right);
+            return l > r;
+        }
+        // double?
+        else if (typeof(TValue) == typeof(double?))
+        {
+            double? l = left as double?;
+            double? r = right as double?;
+            return l.HasValue && r.HasValue && l > r;
+        }
+        // decimal
+        else if (typeof(TValue) == typeof(decimal))
+        {
+            decimal l = Convert.ToDecimal(left);
+            decimal r = Convert.ToDecimal(right);
+            return l > r;
+        }
+        // decimal?
+        else if (typeof(TValue) == typeof(decimal?))
+        {
+            decimal? l = left as decimal?;
+            decimal? r = right as decimal?;
+            return l.HasValue && r.HasValue && l > r;
+        }
+
+        return false;
+    }
+
+    private bool TryParseValue(object value, out TValue newValue)
+    {
+        try
+        {
+            // sbyte? / sbyte
+            if (typeof(TValue) == typeof(sbyte?) || typeof(TValue) == typeof(sbyte))
+            {
+                newValue = (TValue)Convert.ChangeType(value, typeof(sbyte));
+                return true;
+            }
+            // short? / short
+            else if (typeof(TValue) == typeof(short?) || typeof(TValue) == typeof(short))
+            {
+                newValue = (TValue)Convert.ChangeType(value, typeof(short));
+                return true;
+            }
+            // int? / int
+            else if (typeof(TValue) == typeof(int?) || typeof(TValue) == typeof(int))
+            {
+                newValue = (TValue)Convert.ChangeType(value, typeof(int));
+                return true;
+            }
+            // long? / long
+            else if (typeof(TValue) == typeof(long?) || typeof(TValue) == typeof(long))
+            {
+                newValue = (TValue)Convert.ChangeType(value, typeof(long));
+                return true;
+            }
+            // float? / float
+            else if (typeof(TValue) == typeof(float?) || typeof(TValue) == typeof(float))
+            {
+                newValue = (TValue)Convert.ChangeType(value, typeof(float));
+                return true;
+            }
+            // double? / double
+            else if (typeof(TValue) == typeof(double?) || typeof(TValue) == typeof(double))
+            {
+                newValue = (TValue)Convert.ChangeType(value, typeof(double));
+                return true;
+            }
+            // decimal? / decimal
+            else if (typeof(TValue) == typeof(decimal?) || typeof(TValue) == typeof(decimal))
+            {
+                newValue = (TValue)Convert.ChangeType(value, typeof(decimal));
+                return true;
+            }
+
+            newValue = default;
+            return false;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"exception: {ex.Message}");
+            newValue = default;
+            return false;
+        }
     }
 
     #endregion
@@ -157,9 +341,9 @@ public partial class NumberInput : BaseComponent
 
     [CascadingParameter] private EditContext EditContext { get; set; }
 
-    [Parameter] public int? Max { get; set; }
+    [Parameter] public TValue Max { get; set; }
 
-    [Parameter] public int? Min { get; set; }
+    [Parameter] public TValue Min { get; set; }
 
     /// <summary>
     /// Gets or sets the placeholder.
@@ -167,13 +351,13 @@ public partial class NumberInput : BaseComponent
     [Parameter] public string? Placeholder { get; set; }
 
     /// <summary>
-    /// Gets or sets the step.
+    /// Gets or sets the step. Default value is 1.
     /// </summary>
-    [Parameter] public int? Step { get; set; }
+    [Parameter] public double? Step { get; set; } = 1;
 
-    [Parameter] public int? Value { get; set; }
+    [Parameter] public TValue Value { get; set; }
 
-    [Parameter] public Expression<Func<int?>> ValueExpression { get; set; }
+    [Parameter] public Expression<Func<TValue>> ValueExpression { get; set; }
 
     #endregion
 }
