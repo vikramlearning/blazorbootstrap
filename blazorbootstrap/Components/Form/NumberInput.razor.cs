5b13b2d919eb27a9368a58867f5a01dd52f574e5
Vikram Reddy <gvreddy04@hotmail.com>
2022-11-10 08:45:08 +0530

    #62 - NumberInput updates

--- blazorbootstrap/Components/Form/NumberInput.razor.cs
+++ blazorbootstrap/Components/Form/NumberInput.razor.cs
@@ -114,17 +114,15 @@ public partial class NumberInput<TValue> : BaseComponent
 
         if (newValue is null || !TryParseValue(newValue, out TValue value))
             Value = default;
-        else if (Min is not null && IsLeftGreaterThanRight(Min, value)) // value < min
+        else if (EnableMinMax && Min is not null && IsLeftGreaterThanRight(Min, value)) // value < min
             Value = Min;
-        else if (Max is not null && IsLeftGreaterThanRight(value, Max)) // value > max
+        else if (EnableMinMax && Max is not null && IsLeftGreaterThanRight(value, Max)) // value > max
             Value = Max;
         else
             Value = value;
 
         if (oldValue.Equals(Value))
-        {
             await JS.InvokeVoidAsync("window.blazorBootstrap.numberInput.setValue", ElementId, Value);
-        }
 
         await ValueChanged.InvokeAsync(Value);
 
@@ -326,6 +324,8 @@ public partial class NumberInput<TValue> : BaseComponent
     [Parameter] public bool Disabled { get; set; }
 
     [CascadingParameter] private EditContext EditContext { get; set; }
+
+    [Parameter] public bool EnableMinMax { get; set; }
 
     [Parameter] public TValue Max { get; set; }
 
