933a76c0b895bf1e720293831ed9a4c457d8e0ec
Vikram Reddy <gvreddy04@hotmail.com>
2022-12-15 20:31:11 +0530

    AutoComplete demo bug fix.

--- BlazorBootstrap.Demo.Server/Services/CustomerService.cs
+++ BlazorBootstrap.Demo.Server/Services/CustomerService.cs
@@ -1,20 +1,32 @@
-﻿namespace BlazorBootstrap.Demo.Server;
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorBootstrap.Demo.Server;
 
 public class CustomerService : ICustomerService
 {
     private readonly HttpClient _httpClient;
+    private readonly NavigationManager _navigationManager;
 
-    public CustomerService(HttpClient httpClient)
+    public CustomerService(HttpClient httpClient, NavigationManager navigationManager)
     {
         _httpClient = httpClient;
+        _navigationManager= navigationManager;
+        _httpClient.BaseAddress = new Uri(navigationManager.BaseUri);
     }
 
     public async Task<IEnumerable<Customer>> GetCustomers(FilterItem filter)
     {
-        var customers = await _httpClient.GetFromJsonAsync<Customer[]>("sample-data/autocomplete/customer.json");
+        try
+        {
+            var customers = await _httpClient.GetFromJsonAsync<Customer[]>("sample-data/autocomplete/customer.json");
 
-        var parameterExpression = Expression.Parameter(typeof(Customer)); // second param optional
-        var lambda = ExpressionExtensions.GetExpressionDelegate<Customer>(parameterExpression, filter);
-        return customers.Where(lambda.Compile()).OrderBy(customer => customer.CustomerName);
+            var parameterExpression = Expression.Parameter(typeof(Customer)); // second param optional
+            var lambda = ExpressionExtensions.GetExpressionDelegate<Customer>(parameterExpression, filter);
+            return customers.Where(lambda.Compile()).OrderBy(customer => customer.CustomerName);
+        }
+        catch
+        {
+            return new List<Customer>();
+        }
     }
 }
