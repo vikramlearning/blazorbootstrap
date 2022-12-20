b9b2200a84e0582a93c90e3c868b2c24bc9a1e44
Vikram Reddy <gvreddy04@hotmail.com>
2022-12-20 23:46:45 +0530

    Sidebar - draft

---
+++ blazorbootstrap/Components/Sidebar/Sidebar.razor.cs
@@ -0,0 +1,42 @@
+﻿namespace BlazorBootstrap;
+
+public partial class Sidebar : BaseComponent
+{
+    #region Events
+
+
+    #endregion Events
+
+    #region Members
+
+    private DotNetObjectReference<Sidebar> objRef;
+
+    #endregion Members
+
+    #region Methods
+
+    protected override void BuildClasses(ClassBuilder builder)
+    {
+        base.BuildClasses(builder);
+    }
+
+    protected override async Task OnInitializedAsync()
+    {
+        objRef ??= DotNetObjectReference.Create(this);
+
+        Attributes ??= new Dictionary<string, object>();
+
+        await base.OnInitializedAsync();
+
+        //ExecuteAfterRender(async () => { await JS.InvokeVoidAsync("window.blazorBootstrap.autocomplete.initialize", ElementRef, objRef); });
+    }
+
+    #endregion Methods
+
+    #region Properties
+
+    /// <inheritdoc/>
+    protected override bool ShouldAutoGenerateId => true;
+
+    #endregion Properties
+}
