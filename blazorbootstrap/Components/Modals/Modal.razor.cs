f00d51ae7da25d09e7a6b7c76d5cb99657a87423
Vikram Reddy <gvreddy04@hotmail.com>
2023-01-18 18:45:36 +0530

    Modal - Support Dynamic components + ConfirmDialog demo examples update

--- blazorbootstrap/Components/Modals/Modal.razor.cs
+++ blazorbootstrap/Components/Modals/Modal.razor.cs
@@ -1,209 +1,238 @@
-﻿using BlazorBootstrap.Utilities;
-using Microsoft.AspNetCore.Components;
-using Microsoft.JSInterop;
+﻿namespace BlazorBootstrap;
 
-namespace BlazorBootstrap
+public partial class Modal : BaseComponent
 {
-    public partial class Modal : BaseComponent
+    #region Members
+
+    private bool isVisible;
+
+    private string title;
+
+    private Type? childComponent;
+
+    private Dictionary<string, object> parameters;
+
+    private ModalSize size = ModalSize.Regular;
+
+    private ModalFullscreen fullscreen = ModalFullscreen.Disabled;
+
+    private string scrollable => IsScrollable ? "modal-dialog-scrollable" : "";
+
+    private string verticallyCentered => IsVerticallyCentered ? "modal-dialog-centered" : "";
+
+    private string modalSize => BootstrapClassProvider.ToModalSize(Size);
+
+    private string modalFullscreen => BootstrapClassProvider.ToModalFullscreen(Fullscreen);
+
+    private DotNetObjectReference<Modal> objRef;
+
+    #endregion Members
+
+    #region Methods
+
+    protected override void BuildClasses(ClassBuilder builder)
     {
-        #region Members
+        builder.Append(BootstrapClassProvider.Modal());
+        builder.Append(BootstrapClassProvider.ModalFade());
 
-        private ModalSize size = ModalSize.Regular;
+        base.BuildClasses(builder);
+    }
 
-        private ModalFullscreen fullscreen = ModalFullscreen.Disabled;
+    protected override async Task OnInitializedAsync()
+    {
+        this.title = Title;
+        objRef ??= DotNetObjectReference.Create(this);
+        await base.OnInitializedAsync();
 
-        private string scrollable => IsScrollable ? "modal-dialog-scrollable" : "";
+        ExecuteAfterRender(async () => { await JS.InvokeVoidAsync("window.blazorBootstrap.modal.initialize", ElementId, UseStaticBackdrop, CloseOnEscape, objRef); });
+    }
 
-        private string verticallyCentered => IsVerticallyCentered ? "modal-dialog-centered" : "";
+    private async Task ShowAsync(string title, Type? type, Dictionary<string, object> parameters)
+    {
+        this.isVisible = true;
 
-        private string modalSize => BootstrapClassProvider.ToModalSize(Size);
+        if(!string.IsNullOrWhiteSpace(title))
+            this.title = title;
 
-        private string modalFullscreen => BootstrapClassProvider.ToModalFullscreen(Fullscreen);
+        this.childComponent = type;
+        this.parameters = parameters;
+        await JS.InvokeVoidAsync("window.blazorBootstrap.modal.show", ElementId);
+        await InvokeAsync(StateHasChanged);
+    }
 
-        private DotNetObjectReference<Modal> objRef;
+    /// <summary>
+    /// Opens a modal.
+    /// </summary>
+    public async Task ShowAsync()
+    {
+        await ShowAsync(title: null, type: null, parameters: null);
+    }
 
-        #endregion Members
+    /// <summary>
+    /// Opens a modal.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="title"></param>
+    /// <param name="parameters"></param>
+    public async Task ShowAsync<T>(string title, Dictionary<string, object> parameters = null)
+    {
+        await ShowAsync(title: title, type: typeof(T), parameters: parameters);
+    }
 
-        #region Methods
+    /// <summary>
+    /// Hides a modal.
+    /// </summary>
+    public async Task HideAsync()
+    {
+        this.isVisible = false;
+        await JS.InvokeVoidAsync("window.blazorBootstrap.modal.hide", ElementId);
+    }
 
-        protected override void BuildClasses(ClassBuilder builder)
+    /// <inheritdoc />
+    protected override async ValueTask DisposeAsync(bool disposing)
+    {
+        if (disposing)
         {
-            builder.Append(BootstrapClassProvider.Modal());
-            builder.Append(BootstrapClassProvider.ModalFade());
-
-            base.BuildClasses(builder);
+            await JS.InvokeVoidAsync("window.blazorBootstrap.modal.dispose", ElementId);
+            objRef?.Dispose();
         }
 
-        protected override async Task OnInitializedAsync()
-        {
-            objRef ??= DotNetObjectReference.Create(this);
-            await base.OnInitializedAsync();
+        await base.DisposeAsync(disposing);
+    }
 
-            ExecuteAfterRender(async () => { await JS.InvokeVoidAsync("window.blazorBootstrap.modal.initialize", ElementId, UseStaticBackdrop, CloseOnEscape, objRef); });
-        }
+    [JSInvokable] public async Task bsShowModal() => await OnShowing.InvokeAsync();
+    [JSInvokable] public async Task bsShownModal() => await OnShown.InvokeAsync();
+    [JSInvokable] public async Task bsHideModal() => await OnHiding.InvokeAsync();
+    [JSInvokable] public async Task bsHiddenModal() => await OnHidden.InvokeAsync();
+    [JSInvokable] public async Task bsHidePreventedModal() => await OnHidePrevented.InvokeAsync();
 
-        /// <summary>
-        /// Opens a modal.
-        /// </summary>
-        public async Task ShowAsync()
-        {
-            await JS.InvokeVoidAsync("window.blazorBootstrap.modal.show", ElementId);
-        }
+    #endregion Methods
 
-        /// <summary>
-        /// Hides a modal.
-        /// </summary>
-        public async Task HideAsync()
-        {
-            await JS.InvokeVoidAsync("window.blazorBootstrap.modal.hide", ElementId);
-        }
+    #region Properties
 
-        /// <inheritdoc />
-        protected override async ValueTask DisposeAsync(bool disposing)
-        {
-            if (disposing)
-            {
-                await JS.InvokeVoidAsync("window.blazorBootstrap.modal.dispose", ElementId);
-                objRef?.Dispose();
-            }
+    /// <inheritdoc/>
+    protected override bool ShouldAutoGenerateId => true;
 
-            await base.DisposeAsync(disposing);
-        }
+    /// <summary>
+    /// Title in modal header.
+    /// </summary>
+    [Parameter] public string Title { get; set; }
 
-        [JSInvokable] public async Task bsShowModal() => await OnShowing.InvokeAsync();
-        [JSInvokable] public async Task bsShownModal() => await OnShown.InvokeAsync();
-        [JSInvokable] public async Task bsHideModal() => await OnHiding.InvokeAsync();
-        [JSInvokable] public async Task bsHiddenModal() => await OnHidden.InvokeAsync();
-        [JSInvokable] public async Task bsHidePreventedModal() => await OnHidePrevented.InvokeAsync();
-
-        #endregion Methods
-
-        #region Properties
-
-        /// <inheritdoc/>
-        protected override bool ShouldAutoGenerateId => true;
-
-        /// <summary>
-        /// Title in modal header.
-        /// </summary>
-        [Parameter] public string Title { get; set; }
-
-        /// <summary>
+    /// <summary>
 		/// Header template.
 		/// </summary>
 		[Parameter] public RenderFragment HeaderTemplate { get; set; }
 
-        /// <summary>
-        /// Body template.
-        /// </summary>
-        [Parameter] public RenderFragment BodyTemplate { get; set; }
+    /// <summary>
+    /// Body template.
+    /// </summary>
+    [Parameter] public RenderFragment BodyTemplate { get; set; }
 
-        /// <summary>
-        /// Footer template.
-        /// </summary>
-        [Parameter] public RenderFragment FooterTemplate { get; set; }
+    /// <summary>
+    /// Footer template.
+    /// </summary>
+    [Parameter] public RenderFragment FooterTemplate { get; set; }
 
-        /// <summary>
-        /// Size of the modal. Default is <see cref="ModalSize.Regular"/>.
-        /// </summary>
-        [Parameter]
-        public ModalSize Size
+    /// <summary>
+    /// Size of the modal. Default is <see cref="ModalSize.Regular"/>.
+    /// </summary>
+    [Parameter]
+    public ModalSize Size
+    {
+        get => size; 
+        set
         {
-            get => size; 
-            set
-            {
-                size = value;
-                DirtyClasses();
-            }
+            size = value;
+            DirtyClasses();
         }
-
-        /// <summary>
-        /// Fullscreen behavior of the modal. Default is <see cref="ModalFullscreen.Disabled"/>.
-        /// </summary>
-        [Parameter]
-        public ModalFullscreen Fullscreen
-        {
-            get => fullscreen; 
-            set
-            {
-                fullscreen = value;
-                DirtyClasses();
-            }
-        }
-
-        /// <summary>
-        /// Indicates whether the modal shows close button in header.
-        /// Default value is true.
-        /// </summary>
-        [Parameter] public bool ShowCloseButton { get; set; } = true;
-
-        /// <summary>
-        /// Indicates whether the modal closes when escape key is pressed.
-        /// Default value is true.
-        /// </summary>
-        [Parameter] public bool CloseOnEscape { get; set; } = true;
-
-        /// <summary>
-        /// Indicates whether the modal uses a static backdrop.
-        /// Default value is false.
-        /// </summary>
-        [Parameter] public bool UseStaticBackdrop { get; set; } = false;
-
-        /// <summary>
-        /// Allows modal body scroll.
-        /// </summary>
-        [Parameter] public bool IsScrollable { get; set; }
-
-        /// <summary>
-        /// Shows the modal vertically in the center.
-        /// </summary>
-        [Parameter] public bool IsVerticallyCentered { get; set; }
-
-        /// <summary>
-        /// Additional CSS class for the dialog (div.modal-dialog element).
-        /// </summary>
-        [Parameter] public string DialogCssClass { get; set; }
-
-        /// <summary>
-        /// Additional header CSS class.
-        /// </summary>
-        [Parameter] public string HeaderCssClass { get; set; }
-
-        /// <summary>
-        /// Additional body CSS class.
-        /// </summary>
-        [Parameter] public string BodyCssClass { get; set; }
-
-        /// <summary>
-        /// Footer css class.
-        /// </summary>
-        [Parameter] public string FooterCssClass { get; set; }
-
-        /// <summary>
-        /// This event fires immediately when the show instance method is called.
-        /// </summary>
-        [Parameter] public EventCallback OnShowing { get; set; }
-
-        /// <summary>
-        /// This event is fired when an offcanvas element has been made visible to the user (will wait for CSS transitions to complete).
-        /// </summary>
-        [Parameter] public EventCallback OnShown { get; set; }
-
-        /// <summary>
-        /// This event is fired immediately when the hide method has been called.
-        /// </summary>
-        [Parameter] public EventCallback OnHiding { get; set; }
-
-        /// <summary>
-        /// This event is fired when an offcanvas element has been hidden from the user (will wait for CSS transitions to complete).
-        /// </summary>
-        [Parameter] public EventCallback OnHidden { get; set; }
-
-        /// <summary>
-        /// This event is fired when the modal is shown, its backdrop is static and a click outside the modal or an escape key press is performed with the keyboard option or data-bs-keyboard set to false.
-        /// </summary>
-        [Parameter] public EventCallback OnHidePrevented { get; set; }
-
-        #endregion Properties
     }
+
+    /// <summary>
+    /// Fullscreen behavior of the modal. Default is <see cref="ModalFullscreen.Disabled"/>.
+    /// </summary>
+    [Parameter]
+    public ModalFullscreen Fullscreen
+    {
+        get => fullscreen; 
+        set
+        {
+            fullscreen = value;
+            DirtyClasses();
+        }
+    }
+
+    /// <summary>
+    /// Indicates whether the modal shows close button in header.
+    /// Default value is true.
+    /// </summary>
+    [Parameter] public bool ShowCloseButton { get; set; } = true;
+
+    /// <summary>
+    /// Indicates whether the modal closes when escape key is pressed.
+    /// Default value is true.
+    /// </summary>
+    [Parameter] public bool CloseOnEscape { get; set; } = true;
+
+    /// <summary>
+    /// Indicates whether the modal uses a static backdrop.
+    /// Default value is false.
+    /// </summary>
+    [Parameter] public bool UseStaticBackdrop { get; set; } = false;
+
+    /// <summary>
+    /// Allows modal body scroll.
+    /// </summary>
+    [Parameter] public bool IsScrollable { get; set; }
+
+    /// <summary>
+    /// Shows the modal vertically in the center.
+    /// </summary>
+    [Parameter] public bool IsVerticallyCentered { get; set; }
+
+    /// <summary>
+    /// Additional CSS class for the dialog (div.modal-dialog element).
+    /// </summary>
+    [Parameter] public string DialogCssClass { get; set; }
+
+    /// <summary>
+    /// Additional header CSS class.
+    /// </summary>
+    [Parameter] public string HeaderCssClass { get; set; }
+
+    /// <summary>
+    /// Additional body CSS class.
+    /// </summary>
+    [Parameter] public string BodyCssClass { get; set; }
+
+    /// <summary>
+    /// Footer css class.
+    /// </summary>
+    [Parameter] public string FooterCssClass { get; set; }
+
+    /// <summary>
+    /// This event fires immediately when the show instance method is called.
+    /// </summary>
+    [Parameter] public EventCallback OnShowing { get; set; }
+
+    /// <summary>
+    /// This event is fired when an offcanvas element has been made visible to the user (will wait for CSS transitions to complete).
+    /// </summary>
+    [Parameter] public EventCallback OnShown { get; set; }
+
+    /// <summary>
+    /// This event is fired immediately when the hide method has been called.
+    /// </summary>
+    [Parameter] public EventCallback OnHiding { get; set; }
+
+    /// <summary>
+    /// This event is fired when an offcanvas element has been hidden from the user (will wait for CSS transitions to complete).
+    /// </summary>
+    [Parameter] public EventCallback OnHidden { get; set; }
+
+    /// <summary>
+    /// This event is fired when the modal is shown, its backdrop is static and a click outside the modal or an escape key press is performed with the keyboard option or data-bs-keyboard set to false.
+    /// </summary>
+    [Parameter] public EventCallback OnHidePrevented { get; set; }
+
+    #endregion Properties
 }
