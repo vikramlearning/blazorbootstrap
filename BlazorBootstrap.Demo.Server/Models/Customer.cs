eb2a27b744828910e5d862b0ecd0e0e1272f168a
Vikram Reddy <gvreddy04@hotmail.com>
2023-02-10 22:32:47 +0530

    Grid demos update

--- BlazorBootstrap.Demo.Server/Models/Customer.cs
+++ BlazorBootstrap.Demo.Server/Models/Customer.cs
@@ -1,3 +1,14 @@
 ﻿namespace BlazorBootstrap.Demo.Server;
 
 public record Customer(int CustomerId, string CustomerName);
+
+public record Customer2
+(
+    int CustomerId,
+    string CustomerName,
+    string Phone,
+    string Email,
+    string Address,
+    string PostalZip,
+    string Country
+);
