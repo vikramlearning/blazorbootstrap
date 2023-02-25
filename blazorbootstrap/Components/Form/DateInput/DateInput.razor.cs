4bae5840125b4f49432e3854b61df0d8af9a02cb
Vikram Reddy <gvreddy04@gmail.com>
2023-02-26 01:03:58 +0530

    DateInput - draft

---
+++ blazorbootstrap/Components/Form/DateInput/DateInput.razor.cs
@@ -0,0 +1,152 @@
+﻿namespace BlazorBootstrap;
+
+public partial class DateInput<TValue> : BaseComponent
+{
+    #region Events
+
+    /// <summary>
+    /// This event fired on every user keystroke that changes the CurrencyInput value.
+    /// </summary>
+    [Parameter] public EventCallback<TValue> ValueChanged { get; set; }
+
+    #endregion
+
+    #region Members
+
+    private FieldIdentifier fieldIdentifier;
+
+    private string fieldCssClasses => EditContext?.FieldCssClass(fieldIdentifier) ?? "";
+
+    private string autoComplete => this.AutoComplete ? "true" : "false";
+
+    private bool disabled;
+
+    private string formattedValue;
+
+    private bool isFirstRender = true;
+
+    private CultureInfo cultureInfo;
+
+    #endregion
+
+    #region Methods
+
+    protected override void BuildClasses(ClassBuilder builder)
+    {
+        builder.Append(BootstrapClassProvider.FormControl());
+        builder.Append(BootstrapClassProvider.TextAlignment(this.TextAlignment), this.TextAlignment != Alignment.None);
+
+        base.BuildClasses(builder);
+    }
+
+    protected override async Task OnInitializedAsync()
+    {
+        Attributes ??= new Dictionary<string, object>();
+
+        fieldIdentifier = FieldIdentifier.Create(ValueExpression);
+
+        this.disabled = this.Disabled;
+
+        try
+        {
+            this.cultureInfo = new CultureInfo(Locale);
+        }
+        catch (CultureNotFoundException)
+        {
+            this.cultureInfo = new CultureInfo("en-US");
+        }
+
+        await base.OnInitializedAsync();
+    }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            // TODO: update
+        }
+
+        await base.OnAfterRenderAsync(firstRender);
+    }
+
+    /// <summary>
+    /// Disables currency input.
+    /// </summary>
+    public void Disable()
+    {
+        this.disabled = true;
+    }
+
+    /// <summary>
+    /// Enables currency input.
+    /// </summary>
+    public void Enable()
+    {
+        this.disabled = false;
+    }
+
+    private async Task OnChange(ChangeEventArgs e)
+    {
+    }
+
+    #endregion
+
+    #region Properties
+
+    /// <inheritdoc/>
+    protected override bool ShouldAutoGenerateId => true;
+
+    /// <summary>
+    /// Indicates whether the NumberInput can complete the values automatically by the browser.
+    /// </summary>
+    [Parameter] public bool AutoComplete { get; set; }
+
+    /// <summary>
+    /// Gets or sets the disabled.
+    /// </summary>
+    [Parameter] public bool Disabled { get; set; }
+
+    [CascadingParameter] private EditContext EditContext { get; set; }
+
+    /// <summary>
+    /// Gets or sets the locale. Default locale is 'en-US'.
+    /// </summary>
+    [Parameter, EditorRequired] public string Locale { get; set; } = "en-US";
+
+    /// <summary>
+    /// Gets or sets the max.
+    /// Allowed format is yyyy-mm-dd.
+    /// </summary>
+    [Parameter] public string Max { get; set; }
+
+    /// <summary>
+    /// Gets or sets the min.
+    /// Allowed format is yyyy-mm-dd.
+    /// </summary>
+    [Parameter] public string Min { get; set; }
+
+    /// <summary>
+    /// Gets or sets the placeholder.
+    /// </summary>
+    [Parameter] public string? Placeholder { get; set; }
+
+    /// <summary>
+    /// Gets or sets the step.
+    /// The default value of step is 1, indicating 1 day.
+    /// </summary>
+    [Parameter] public int Step { get; set; } = 1;
+
+    /// <summary>
+    /// Gets or sets the text alignment.
+    /// </summary>
+    [Parameter] public Alignment TextAlignment { get; set; }
+
+    /// <summary>
+    /// Gets or sets the value.
+    /// </summary>
+    [Parameter] public TValue Value { get; set; } = default!;
+
+    [Parameter] public Expression<Func<TValue>> ValueExpression { get; set; } = default!;
+
+    #endregion
+}
