0631f3473efb4dfd90f4c637545f6afb26691301
Vikram Reddy <gvreddy04@gmail.com>
2023-04-03 01:01:06 +0530

    Accordion - initial setup

---
+++ blazorbootstrap/Components/Accordion/Accordion.razor.cs
@@ -0,0 +1,43 @@
+﻿namespace BlazorBootstrap;
+
+public partial class Accordion
+{
+    #region Events
+
+    #endregion
+
+    #region Members
+
+    #endregion
+
+    #region Methods
+
+    /// <inheritdoc/>
+    protected override void BuildClasses(ClassBuilder builder)
+    {
+        base.BuildClasses(builder);
+    }
+
+    #endregion
+
+    #region Properties
+
+    /// <inheritdoc/>
+    protected override bool ShouldAutoGenerateId => true;
+
+    /// <summary>
+    /// Specifies the content to be rendered inside this <see cref="Collapse"/>.
+    /// </summary>
+    [Parameter, EditorRequired]
+    public RenderFragment ChildContent { get; set; } = default!;
+
+    /// <summary>
+    /// Gets or sets the parent.
+    /// If parent is provided, then all collapsible elements under the specified parent will be closed when this collapsible item is shown. (similar to traditional accordion behavior - this is dependent on the card class). 
+    /// The attribute has to be set on the target collapsible area.
+    /// </summary>
+    [Parameter] public string Parent { get; set; } = default!;
+
+    #endregion
+}
+
