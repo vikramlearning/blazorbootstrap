65672f56806210b4ee6ce930fff7e33a5b64cfc3
Vikram Reddy <gvreddy04@gmail.com>
2023-06-11 21:36:57 +0530

    Demos updated.

---
+++ BlazorBootstrap.Demo.Server/Models/Employee4.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorBootstrap.Demo.Server;
+
+public record class Employee4
+{
+    public int? Id { get; set; }
+    public string Name { get; set; }
+    public string Designation { get; set; }
+    public DateOnly? DOJ { get; set; }
+    public bool IsActive { get; set; }
+}
