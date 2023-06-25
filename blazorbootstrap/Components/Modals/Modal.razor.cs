b7c406f92e27abfc9c56e2b84b57d608358e9a18
Vikram Reddy <gvreddy04@gmail.com>
2023-06-25 21:31:43 +0530

    Modal service initial version

--- blazorbootstrap/Components/Modals/Modal.razor.cs
+++ blazorbootstrap/Components/Modals/Modal.razor.cs
@@ -6,11 +6,13 @@ public partial class Modal : BaseComponent
 
     private bool isVisible;
 
-    private string title;
+    private string title = default!;
 
-    private Type? childComponent;
+    private string message = default!;
 
-    private Dictionary<string, object> parameters;
+    private Type? childComponent = default!;
+
+    private Dictionary<string, object> parameters = default!;
 
     private ModalSize size = ModalSize.Regular;
 
@@ -24,7 +26,15 @@ public partial class Modal : BaseComponent
 
     private string modalFullscreen => BootstrapClassProvider.ToModalFullscreen(Fullscreen);
 
-    private DotNetObjectReference<Modal> objRef;
+    private bool showFooterButton = false;
+
+    private string footerButtonText = string.Empty;
+
+    private ButtonColor footerButtonColor = ButtonColor.Secondary;
+
+    private string footerButtonCSSClass = string.Empty;
+
+    private DotNetObjectReference<Modal> objRef = default!;
 
     #endregion Members
 
@@ -40,19 +50,48 @@ public partial class Modal : BaseComponent
 
     protected override async Task OnInitializedAsync()
     {
-        this.title = Title;
+        if (ModalService is not null)
+            ModalService.OnShow += OnShowAsync;
+
         objRef ??= DotNetObjectReference.Create(this);
         await base.OnInitializedAsync();
 
         ExecuteAfterRender(async () => { await JS.InvokeVoidAsync("window.blazorBootstrap.modal.initialize", ElementId, UseStaticBackdrop, CloseOnEscape, objRef); });
     }
 
+    #region Modal Service Events
+
+    private Task OnShowAsync(ModalOption modalOption)
+    {
+        if (modalOption.Type == ModalType.Success)
+            HeaderCssClass = "text-bg-success border-bottom border-success";
+        else if (modalOption.Type == ModalType.Danger)
+            HeaderCssClass = "text-bg-danger border-bottom border-danger";
+        else if (modalOption.Type == ModalType.Warning)
+            HeaderCssClass = "text-bg-warning border-bottom border-warning";
+        else if (modalOption.Type == ModalType.Info)
+            HeaderCssClass = "text-bg-info border-bottom border-info";
+        else
+            HeaderCssClass = "";
+
+        showFooterButton = modalOption.ShowFooterButton;
+        if (showFooterButton)
+        {
+            footerButtonText = modalOption.FooterButtonText;
+            FooterCssClass = "border-top-0";
+        }
+
+        return ShowAsync(title: modalOption.Title, message: modalOption.Message, type: null, parameters: null);
+    }
+
+    #endregion
+
     /// <summary>
     /// Opens a modal.
     /// </summary>
     public async Task ShowAsync()
     {
-        await ShowAsync(title: null, type: null, parameters: null);
+        await ShowAsync(title: null, message: null, type: null, parameters: null);
     }
 
     /// <summary>
@@ -60,21 +99,26 @@ public partial class Modal : BaseComponent
     /// </summary>
     /// <typeparam name="T"></typeparam>
     /// <param name="title"></param>
+    /// <param name="message"></param>
     /// <param name="parameters"></param>
-    public async Task ShowAsync<T>(string title, Dictionary<string, object> parameters = null)
+    public async Task ShowAsync<T>(string title, string message = null, Dictionary<string, object> parameters = null)
     {
-        await ShowAsync(title: title, type: typeof(T), parameters: parameters);
+        await ShowAsync(title: title, message: message, type: typeof(T), parameters: parameters);
     }
 
-    private async Task ShowAsync(string title, Type? type, Dictionary<string, object> parameters)
+    private async Task ShowAsync(string title, string message, Type? type, Dictionary<string, object> parameters)
     {
         this.isVisible = true;
 
         if (!string.IsNullOrWhiteSpace(title))
-            this.title = title;
+            this.Title = title;
+
+        if (!string.IsNullOrWhiteSpace(message))
+            this.Message = message;
 
         this.childComponent = type;
         this.parameters = parameters;
+
         await JS.InvokeVoidAsync("window.blazorBootstrap.modal.show", ElementId);
         await InvokeAsync(StateHasChanged);
     }
@@ -116,22 +160,37 @@ public partial class Modal : BaseComponent
     /// <summary>
     /// Title in modal header.
     /// </summary>
-    [Parameter] public string Title { get; set; }
+    [Parameter]
+    public string Title
+    {
+        get => title;
+        set => title = value;
+    }
 
     /// <summary>
     /// Header template.
     /// </summary>
-    [Parameter] public RenderFragment HeaderTemplate { get; set; }
+    [Parameter] public RenderFragment HeaderTemplate { get; set; } = default!;
+
+    /// <summary>
+    /// Message in modal body.
+    /// </summary>
+    [Parameter]
+    public string Message
+    {
+        get => message;
+        set => message = value;
+    }
 
     /// <summary>
     /// Body template.
     /// </summary>
-    [Parameter] public RenderFragment BodyTemplate { get; set; }
+    [Parameter] public RenderFragment BodyTemplate { get; set; } = default!;
 
     /// <summary>
     /// Footer template.
     /// </summary>
-    [Parameter] public RenderFragment FooterTemplate { get; set; }
+    [Parameter] public RenderFragment FooterTemplate { get; set; } = default!;
 
     /// <summary>
     /// Size of the modal. Default is <see cref="ModalSize.Regular"/>.
@@ -192,22 +251,22 @@ public partial class Modal : BaseComponent
     /// <summary>
     /// Additional CSS class for the dialog (div.modal-dialog element).
     /// </summary>
-    [Parameter] public string DialogCssClass { get; set; }
+    [Parameter] public string DialogCssClass { get; set; } = default!;
 
     /// <summary>
     /// Additional header CSS class.
     /// </summary>
-    [Parameter] public string HeaderCssClass { get; set; }
+    [Parameter] public string HeaderCssClass { get; set; } = default!;
 
     /// <summary>
     /// Additional body CSS class.
     /// </summary>
-    [Parameter] public string BodyCssClass { get; set; }
+    [Parameter] public string BodyCssClass { get; set; } = default!;
 
     /// <summary>
     /// Footer css class.
     /// </summary>
-    [Parameter] public string FooterCssClass { get; set; }
+    [Parameter] public string FooterCssClass { get; set; } = default!;
 
     /// <summary>
     /// This event fires immediately when the show instance method is called.
@@ -240,4 +299,10 @@ public partial class Modal : BaseComponent
     [Parameter] public int TabIndex { get; set; } = -1;
 
     #endregion Properties
+
+    #region Services
+
+    [Inject] public ModalService ModalService { get; set; } = default!;
+
+    #endregion
 }
---
+++ blazorbootstrap/Services/ModalService.cs
@@ -0,0 +1,30 @@
+﻿namespace BlazorBootstrap;
+
+public class ModalService
+{
+    internal event Func<ModalOption, Task> OnShow = default!;
+    internal event Func<ModalOption, Task> OnHide = default!;
+
+    public Task ShowAsync(ModalOption modalOption) => OnShow?.Invoke(modalOption);
+    public Task HideAsync(ModalOption modalOption) => OnHide?.Invoke(modalOption);
+}
+
+public class ModalOption
+{
+    public string Title { get; set; } = default!;
+    public string Message { get; set; } = default!;
+    public ModalType Type { get; set; }
+    public bool ShowFooterButton { get; set; } = true;
+    public string FooterButtonText { get; set; } = "OK";
+    public ButtonColor FooterButtonColor { get; set; }
+    public string FooterButtonCSSClass { get; set; } = default!;
+}
+
+public enum ModalType
+{
+    Default,
+    Success,
+    Danger,
+    Warning,
+    Info
+}
