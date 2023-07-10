0d8a6583bf26f41c7292447a8ff2928a5dba3d97
Vikram Reddy <gvreddy04@gmail.com>
2023-07-10 18:10:59 +0530

    Grid Component: Row Click Event + Row Double Click Event (#270)
    * Grid Component: Row Click Event #264

---
+++ blazorbootstrap/EventArguments/GridRowEventArgs.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorBootstrap;
+
+public class GridRowEventArgs<TItem> : EventArgs
+{
+    public GridRowEventArgs(TItem item)
+    {
+        this.Item = item;
+    }
+
+    public TItem Item { get; set; }
+}
