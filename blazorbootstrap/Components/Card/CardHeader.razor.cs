75805e277eae4f0308270e300e3cd43c96fab1c7
Eike Borth <6439501+TechosRX@users.noreply.github.com>
2023-11-02 16:14:25 +0100

    Added CardColor to CardHeader (#410)
    * Added CardColor to CardHeader
    
    * Add CardHeader Color to card.md Documentation
    
    ---------
    
    Co-authored-by: Eike Borth <skript.hoehepunkt.0l@icloud.com>
    Co-authored-by: Eike Borth <techos@animexx.de>

--- blazorbootstrap/Components/Card/CardHeader.razor.cs
+++ blazorbootstrap/Components/Card/CardHeader.razor.cs
@@ -8,6 +8,7 @@ public partial class CardHeader
     protected override void BuildClasses(CssClassBuilder builder)
     {
         builder.Append(BootstrapClassProvider.CardHeader());
+        builder.Append(BootstrapClassProvider.ToCardColor(Color));
 
         base.BuildClasses(builder);
     }
@@ -24,6 +25,12 @@ public partial class CardHeader
     /// </summary>
     [Parameter]
     public RenderFragment ChildContent { get; set; } = default!;
+
+    /// <summary>
+    /// Gets or sets the card header color.
+    /// </summary>
+    [Parameter]
+    public CardColor Color { get; set; }
 
     #endregion
 }
