6912a91b13b66b7c28c12a3d24290e315b113548
Vikram Gaddam <gvreddy04@gmail.com>
2023-11-19 00:28:37 +0530

    Carbon ads (#433)
    * Enable Carbon Ads

---
+++ BlazorBootstrap.Demo.RCL/Shared/Components/CarbonAds.razor.cs
@@ -0,0 +1,21 @@
+﻿namespace BlazorBootstrap.Demo.RCL;
+
+public partial class CarbonAds : ComponentBase
+{
+    #region Fields and Constants
+
+    private string? classNames;
+
+    #endregion
+
+    #region Properties, Indexers
+
+    [Parameter]
+    public string? ClassNames
+    {
+        get => classNames;
+        set => classNames = $"mt-5 {value ?? ""}".Trim();
+    }
+
+    #endregion
+}
