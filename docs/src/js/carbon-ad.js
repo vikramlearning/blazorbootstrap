cd3c4ad4923413ee39532e3a88f1a1bb53cc0c0a
Vikram Gaddam <gvreddy04@gmail.com>
2023-12-02 22:27:16 +0530

    Docs update (#448)
    * Carbon Ads - Docs update

--- docs/src/js/carbon-ad.js
+++ docs/src/js/carbon-ad.js
@@ -3,21 +3,21 @@ import ReactDOM from 'react-dom'
 
 export function RenderAd() {
     let scriptLoaderEl = document.getElementById(`divCarbonAd`);
-    let scriptEl = document.getElementById(`_carbonads_js`);
-    if (scriptLoaderEl) {
-        if (scriptEl) {
-            // do nothing
-        } else {
-            scriptEl = document.createElement('script');
-            scriptEl.async = true;
-            scriptEl.id = `_carbonads_js`;
-            scriptEl.src = `//cdn.carbonads.com/carbon.js?serve=CWYICKJI&placement=docsblazorbootstrapcom`;
-            scriptEl.type = `text/javascript`;            
-            scriptEl.onerror = function () {
-                console.error(`An error occurred while loading the script: ${source}`);
-            }
-            scriptLoaderEl.appendChild(scriptEl);
+    if (scriptLoaderEl) {     
+        let scriptEl = document.getElementById(`_carbonads_js`);   
+        if (scriptEl) {            
+            scriptEl.remove();
         }
+        scriptEl = document.createElement('script');
+        scriptEl.async = true;
+        scriptEl.id = `_carbonads_js`;
+        scriptEl.src = `//cdn.carbonads.com/carbon.js?serve=CWYICKJI&placement=docsblazorbootstrapcom`;
+        scriptEl.type = `text/javascript`;            
+        scriptEl.onerror = function () {
+            console.error(`An error occurred while loading the script: ${source}`);
+        }
+        scriptLoaderEl.replaceChildren();
+        scriptLoaderEl.appendChild(scriptEl);
     }
     return null;
 }
