8b3110dee6b5c658131bc4b10667f3d33a9b706e
Vikram Reddy <gvreddy04@gmail.com>
2023-12-03 16:44:39 +0530

    Carbon ads update

--- docs/src/js/carbon-ad.js
+++ docs/src/js/carbon-ad.js
@@ -1,27 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom'
+import React, { Component } from 'react';
 
-export function RenderAd() {
-    let scriptLoaderEl = document.getElementById(`divCarbonAd`);
-    if (scriptLoaderEl) {
-        scriptLoaderEl.replaceChildren();
-        setTimeout(() => {
-            let scriptEl = document.getElementById(`_carbonads_js`);
-            if (scriptEl) {
-                scriptEl.remove();
-            }
-            scriptEl = document.createElement('script');
-            scriptEl.async = true;
-            scriptEl.id = `_carbonads_js`;
-            scriptEl.src = `//cdn.carbonads.com/carbon.js?serve=CWYICKJI&placement=docsblazorbootstrapcom&ms=${(new Date()).getUTCMilliseconds()}`;
-            scriptEl.type = `text/javascript`;
-            scriptEl.onerror = function () {
-                console.error(`An error occurred while loading the script: ${source}`);
-            }
-            if (scriptEl) {
-                scriptLoaderEl.appendChild(scriptEl);
-            }
-        }, 5000);
+class CarbonAd extends Component {
+  constructor(props) {
+    super(props);
+
+    this.name = this.props.name || "docsblazorbootstrapcom";
+    this.serve = this.props.serve || "CWYICKJI";
+    this.script = this.props.script || null;
+    this.placement = this.props.placement || "";
+    this.fallback = this.props.fallback || null;
+    this.showFallback = false;
+  }
+
+  adShowing = () => document.getElementById(`${this.name} #carbonads`) !== null;
+
+  componentDidMount = () => {
+    let script = document.createElement("script");
+    script.defer = !!this.script;
+    script.async = true;
+    script.id = this.script ? "" : "_carbonads_js";
+    script.type = "text/javascript";
+    script.src =
+      this.script ||
+      `//cdn.carbonads.com/carbon.js?serve=${this.serve}&placement=${
+        this.placement
+      }`;
+    script.onerror = () => {
+      this.showFallback = true;
+      this.forceUpdate();
+    };
+    script.addEventListener("load", () => {
+      if (!this.adShowing) _.invoke(window._carbonads, "refresh");
+    });
+
+    document.querySelector(`#${this.name}`).appendChild(script);
+  };
+
+  render() {
+    if (this.showFallback && this.fallback) {
+      return this.fallback;
     }
-    return null;
+    return <div id={this.name} />;
+  }
 }
+
+export default CarbonAd;
