fd9aa32df6d4e902310ce04ae4ed1edba7c13dc3
Vikram Reddy <gvreddy04@gmail.com>
2023-12-03 10:11:03 +0530

    Carbon ads - delay added.

--- docs/src/js/carbon-ad.js
+++ docs/src/js/carbon-ad.js
@@ -3,21 +3,25 @@ import ReactDOM from 'react-dom'
 
 export function RenderAd() {
     let scriptLoaderEl = document.getElementById(`divCarbonAd`);
-    if (scriptLoaderEl) {     
-        let scriptEl = document.getElementById(`_carbonads_js`);   
-        if (scriptEl) {            
-            scriptEl.remove();
-        }
-        scriptEl = document.createElement('script');
-        scriptEl.async = true;
-        scriptEl.id = `_carbonads_js`;
-        scriptEl.src = `//cdn.carbonads.com/carbon.js?serve=CWYICKJI&placement=docsblazorbootstrapcom`;
-        scriptEl.type = `text/javascript`;            
-        scriptEl.onerror = function () {
-            console.error(`An error occurred while loading the script: ${source}`);
-        }
-        scriptLoaderEl.replaceChildren();
-        scriptLoaderEl.appendChild(scriptEl);
+    if (scriptLoaderEl) {
+        setTimeout(() => {
+            let scriptEl = document.getElementById(`_carbonads_js`);
+            if (scriptEl) {
+                scriptEl.remove();
+            }
+            scriptEl = document.createElement('script');
+            scriptEl.async = true;
+            scriptEl.id = `_carbonads_js`;
+            scriptEl.src = `//cdn.carbonads.com/carbon.js?serve=CWYICKJI&placement=docsblazorbootstrapcom`;
+            scriptEl.type = `text/javascript`;
+            scriptEl.onerror = function () {
+                console.error(`An error occurred while loading the script: ${source}`);
+            }
+            scriptLoaderEl.replaceChildren();
+            if (scriptEl) {
+                scriptLoaderEl.appendChild(scriptEl);
+            }
+        }, 5000);
     }
     return null;
 }
