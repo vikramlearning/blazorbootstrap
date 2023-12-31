e385e6e987aa6fad4a880d0d384837d9b33baefd
Vikram Gaddam <gvreddy04@gmail.com>
2023-12-31 11:35:45 +0530

    toast dispose issue (#488)
    * Using Toast Service Giving Error #487

--- blazorbootstrap/wwwroot/blazor.bootstrap.js
+++ blazorbootstrap/wwwroot/blazor.bootstrap.js
@@ -648,9 +648,15 @@ window.blazorBootstrap = {
                 dotNetHelper.invokeMethodAsync('bsShownToast');
             });
             toastEl.addEventListener('hide.bs.toast', function () {
+                let _this = this;
+                if (_this == null || document.getElementById(_this.id) == null) // when a user is redirected to a different page, the HTML element becomes unavailable.
+                    return;
                 dotNetHelper.invokeMethodAsync('bsHideToast');
             });
             toastEl.addEventListener('hidden.bs.toast', function () {
+                let _this = this;
+                if (_this == null || document.getElementById(_this.id) == null) // when a user is redirected to a different page, the HTML element becomes unavailable.
+                    return;
                 dotNetHelper.invokeMethodAsync('bsHiddenToast');
             });
 
