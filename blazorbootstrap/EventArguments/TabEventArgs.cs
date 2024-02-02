6ded1a5e021fb9dd6042c1ff969f3dbc7ebf39db
Vikram Reddy <gvreddy04@gmail.com>
2024-02-02 23:22:15 +0530

    Tabs demos + docs update

---
+++ blazorbootstrap/EventArguments/TabEventArgs.cs
@@ -0,0 +1,28 @@
+﻿namespace BlazorBootstrap;
+
+public class TabEventArgs
+{
+    #region Constructors
+
+    public TabEventArgs(string name, string title)
+    {
+        Name = name;
+        Title = title;
+    }
+
+    #endregion
+
+    #region Properties, Indexers
+
+    /// <summary>
+    /// Gets the <see cref="Tab" /> name.
+    /// </summary>
+    public string Name { get; }
+
+    /// <summary>
+    /// Gets the <see cref="Tab" /> title.
+    /// </summary>
+    public string Title { get; }
+
+    #endregion
+}
