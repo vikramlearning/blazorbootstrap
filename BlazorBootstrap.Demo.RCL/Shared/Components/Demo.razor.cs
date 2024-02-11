dcca39f5c8b7515cc011f61957938ca54e85837a
Vikram Reddy <gvreddy04@gmail.com>
2024-02-11 17:21:46 +0530

    Demo component - refactor

---
+++ BlazorBootstrap.Demo.RCL/Shared/Components/Demo.razor.cs
@@ -0,0 +1,59 @@
+﻿namespace BlazorBootstrap.Demo.RCL;
+
+public partial class Demo : ComponentBase
+{
+    #region Members
+
+    private string? codeSnippet;
+
+    #endregion
+
+    #region Methods
+
+    protected override async Task OnParametersSetAsync()
+    {
+        if (codeSnippet is null)
+        {
+            var resourceName = Type.FullName + ".razor";
+            using (Stream stream = Type.Assembly.GetManifestResourceStream(resourceName)!)
+            {
+                try
+                {
+                    if (stream is null)
+                        return;
+
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        codeSnippet = await reader.ReadToEndAsync();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        await base.OnAfterRenderAsync(firstRender);
+        await JS.InvokeVoidAsync("highlightCode");
+    }
+
+    #endregion
+
+    #region Properties
+
+    [Inject] protected IJSRuntime JS { get; set; } = default!;
+
+    [Parameter] public string LanguageCssClass { get; set; } = "language-cshtml";
+
+    [Parameter] public bool ShowCodeOnly { get; set; }
+
+    [Parameter] public bool Tabs { get; set; } = false;
+
+    [Parameter] public Type Type { get; set; } = default!;
+
+    #endregion
+}
