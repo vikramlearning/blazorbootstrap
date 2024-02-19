e71d2dead6e7488d04cfbcf86e2c0898c15e6d26
Vikram Reddy <gvreddy04@gmail.com>
2024-02-19 09:59:54 +0530

    Create seperate files for the classes.

--- blazorbootstrap/Components/Core/BlazorBootstrapComponentBase.cs
+++ blazorbootstrap/Components/Core/BlazorBootstrapComponentBase.cs
@@ -389,28 +389,3 @@ public abstract class BlazorBootstrapComponentBase : ComponentBase, IDisposable,
     
     #endregion
 }
-
-public record RenderPriority
-{
-    public RenderPriority()
-    {
-        Priority = Priority.Low;
-        Order = DateTime.Now.Ticks;
-    }
-
-    public RenderPriority(Priority priority)
-    {
-        Priority = priority;
-    }
-
-    public Priority Priority { get; init; }
-
-    public long Order { get; init; }
-}
-
-public enum Priority
-{
-    High = 0,
-    Medium,
-    Low
-}
