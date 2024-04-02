23de18af741f0050de3d33fe8f5a2fd4536baab3
MarvinKlein1508 <support@marvinkleinmusic.de>
2024-04-02 15:20:23 +0200

    Update demo to Sidebar3

--- BlazorBootstrap.Demo.RCL/Shared/MainLayout.razor.cs
+++ BlazorBootstrap.Demo.RCL/Shared/MainLayout.razor.cs
@@ -3,8 +3,8 @@
 public partial class MainLayout : LayoutComponentBase
 {
     private string version = default!;
-    private Sidebar sidebar = default!;
-    private IEnumerable<NavItem> navItems = default!;
+    private Sidebar3 sidebar = default!;
+    private IEnumerable<Sidebar3NavItem> navItems = default!;
 
     [Inject] public IConfiguration Configuration { get; set; } = default!;
 
@@ -14,7 +14,7 @@ public partial class MainLayout : LayoutComponentBase
         base.OnInitialized();
     }
 
-    private async Task<SidebarDataProviderResult> SidebarDataProvider(SidebarDataProviderRequest request)
+    private async Task<Sidebar3DataProviderResult> SidebarDataProvider(Sidebar3DataProviderRequest request)
     {
         if (navItems is null)
             navItems = GetNavItems();
@@ -22,66 +22,325 @@ public partial class MainLayout : LayoutComponentBase
         return await Task.FromResult(request.ApplyTo(navItems));
     }
 
-    private IEnumerable<NavItem> GetNavItems()
+    private IEnumerable<Sidebar3NavItem> GetNavItems()
     {
-        navItems = new List<NavItem>
+        return new List<Sidebar3NavItem>
         {
-            new (){ Id = "1", Text = "Getting Started", Href = "/getting-started", IconName = IconName.HouseDoorFill },
 
-            new (){ Id = "2", Text = "Layout", IconName = IconName.LayoutTextWindowReverse, IconColor = IconColor.Success },
-            new (){ Id = "200", Text = "Blazor WebAssembly", Href = "/layout-setup/blazor-webassembly", IconName = IconName.BrowserEdge, ParentId = "2" },
-            new (){ Id = "201", Text = "Blazor Server", Href = "/layout-setup/blazor-server", IconName = IconName.Server, ParentId = "2" },
+            new ()
+            {
+                Text = "Getting Started",
+                Href = "/getting-started",
+                IconName = IconName.HouseDoorFill
+            },
+            new ()
+            {
+                Text = "Layout",
+                IconName = IconName.LayoutTextWindowReverse,
+                IconColor = IconColor.Success,
+                ChildItems = new List<Sidebar3NavItem>()
+                {
+                    new()
+                    {
+                        Text = "Blazor WebAssembly",
+                        Href = "/layout-setup/blazor-webassembly",
+                        IconName = IconName.BrowserEdge
+                    },
+                    new()
+                    {
+                        Text = "Blazor Server",
+                        Href = "/layout-setup/blazor-server",
+                        IconName = IconName.Server
+                    }
+                }
+            },
+            new ()
+            { 
+                Text = "Content", 
+                IconName = IconName.BodyText, 
+                IconColor = IconColor.Primary,
+                ChildItems = new List<Sidebar3NavItem>
+                {
+                    new()
+                    {
+                        Text = "Icons", 
+                        Href = "/icons", 
+                        IconName = IconName.PersonSquare
+                    }
+                }
+            },
+            new ()
+            { 
+                Text = "Forms", 
+                IconName = IconName.InputCursorText, 
+                IconColor = IconColor.Success,
+                ChildItems = new List<Sidebar3NavItem>()
+                {
+                    new ()
+                    { 
+                        Text = "Auto Complete", 
+                        Href = "/autocomplete", 
+                        IconName = IconName.InputCursorText
+                    },
+                    new ()
+                    { 
+                        Text = "Currency Input", 
+                        Href = "/form/currency-input", 
+                        IconName = IconName.CurrencyDollar
+                    },
+                    new ()
+                    { 
+                        Text = "Date Input", 
+                        Href = "/form/date-input", 
+                        IconName = IconName.CalendarDate
+                    },
+                    new ()
+                    { 
+                        Text = "Number Input", 
+                        Href = "/form/number-input", 
+                        IconName = IconName.InputCursor
+                    },
+                    new ()
+                    { 
+                        Text = "Range Input", 
+                        Href = "/form/range-input", 
+                        IconName = IconName.Sliders 
+                    },
+                    new ()
+                    { 
+                        Text = "Switch", 
+                        Href = "/form/switch", 
+                        IconName = IconName.ToggleOn 
+                    },
+                    new ()
+                    { 
+                        Text = "Time Input", 
+                        Href = "/form/time-input", 
+                        IconName = IconName.ClockFill
+                    },
 
-            new (){ Id = "3", Text = "Content", IconName = IconName.BodyText, IconColor = IconColor.Primary },
-            new (){ Id = "300", Text = "Icons", Href = "/icons", IconName = IconName.PersonSquare, ParentId = "3" },
+                }
+            },
+            new ()
+            {  
+                Text = "Components", 
+                IconName = IconName.GearFill, 
+                IconColor = IconColor.Danger,
+                ChildItems = new List<Sidebar3NavItem>()
+                {
+                    new ()
+                    { 
+                        Text = "Accordion",
+                        Href = "/accordion",
+                        IconName = IconName.ChevronBarExpand
+                    },
+                    new ()
+                    { 
+                        Text = "Alerts",
+                        Href = "/alerts",
+                        IconName = IconName.CheckCircleFill
+                    },
+                    new ()
+                    { 
+                        Text = "Badge",
+                        Href = "/badge",
+                        IconName = IconName.AppIndicator
+                    },
+                    new ()
+                    { 
+                        Text = "Breadcrumb",
+                        Href = "/breadcrumb",
+                        IconName = IconName.SegmentedNav
+                    },
+                    new ()
+                    { 
+                        Text = "Buttons",
+                        Href = "/buttons",
+                        IconName = IconName.ToggleOn
+                    },
+                    new ()
+                    { 
+                        Text = "Callout",
+                        Href = "/callout",
+                        IconName = IconName.StickyFill
+                    },
+                    new ()
+                    { 
+                        Text = "Card",
+                        Href = "/card",
+                        IconName = IconName.CardHeading
+                    },
+                    new ()
+                    { 
+                        Text = "Charts",
+                        Href = "/charts",
+                        IconName = IconName.BarChartLineFill, 
+                        Match = NavLinkMatch.All 
+                    },
+                    new ()
+                    { 
+                        Text = "Collapse",
+                        Href = "/collapse",
+                        IconName = IconName.ArrowsCollapse
+                    },
+                    new ()
+                    { 
+                        Text = "Confirm Dialog", 
+                        Href = "/confirm-dialog", 
+                        IconName = IconName.QuestionDiamondFill
+                    },
+                    new ()
+                    { 
+                        Text = "Dropdown",
+                        Href = "/dropdown",
+                        IconName = IconName.MenuButtonWideFill
+                    },
+                    new ()
+                    { 
+                        Text = "Grid",
+                        Href = "/grid",
+                        IconName = IconName.Grid
+                    },
+                    new ()
+                    { 
+                        Text = "Modals",
+                        Href = "/modals",
+                        IconName = IconName.WindowStack
+                    },
+                    new ()
+                    { 
+                        Text = "Offcanvas",
+                        Href = "/offcanvas",
+                        IconName = IconName.LayoutSidebarReverse
+                    },
+                    new ()
+                    { 
+                        Text = "Pagination",
+                        Href = "/pagination",
+                        IconName = IconName.ThreeDots
+                    },
+                    new ()
+                    { 
+                        Text = "Pdf Viewer", 
+                        Href = "/pdf-viewer", 
+                        IconName = IconName.FilePdfFill, 
+                    },
+                    new ()
+                    { 
+                        Text = "Placeholders",
+                        Href = "/placeholders",
+                        IconName = IconName.ColumnsGap
+                    },
+                    new ()
+                    { 
+                        Text = "Preload",
+                        Href = "/preload",
+                        IconName = IconName.ArrowClockwise 
+                    },
+                    new ()
+                    { 
+                        Text = "Progress",
+                        Href = "/progress",
+                        IconName = IconName.UsbC },
+                    new ()
+                    { 
+                        Text = "Script Loader", 
+                        Href = "/script-loader", 
+                        IconName = IconName.CodeSlash
+                    },
+                    new ()
+                    { 
+                        Text = "Sidebar",
+                        Href = "/sidebar",
+                        IconName = IconName.LayoutSidebar
+                    },
+                    new ()
+                    { 
+                        Text = "Sidebar 2", 
+                        Href = "/sidebar2", 
+                        IconName = IconName.ListNested
+                    },
+                    new ()
+                    { 
+                        Text = "Spinner",
+                        Href = "/spinners",
+                        IconName = IconName.ArrowRepeat 
+                    },
+                    new ()
+                    { 
+                        Text = "Tabs",
+                        Href = "/tabs",
+                        IconName = IconName.WindowPlus 
+                    },
+                    new ()
+                    { 
+                        Text = "Toasts",
+                        Href = "/toasts",
+                        IconName = IconName.ExclamationTriangleFill 
+                    },
+                    new ()
+                    { 
+                        Text = "Tooltips",
+                        Href = "/tooltips",
+                        IconName = IconName.ChatSquareDotsFill
+                    },
 
-            new (){ Id = "4", Text = "Forms", IconName = IconName.InputCursorText, IconColor = IconColor.Success },
-            new (){ Id = "400", Text = "Auto Complete", Href = "/autocomplete", IconName = IconName.InputCursorText, ParentId = "4" },
-            new (){ Id = "401", Text = "Currency Input", Href = "/form/currency-input", IconName = IconName.CurrencyDollar, ParentId = "4" },
-            new (){ Id = "402", Text = "Date Input", Href = "/form/date-input", IconName = IconName.CalendarDate, ParentId = "4" },
-            new (){ Id = "403", Text = "Number Input", Href = "/form/number-input", IconName = IconName.InputCursor, ParentId = "4" },
-            new (){ Id = "404", Text = "Range Input", Href = "/form/range-input", IconName = IconName.Sliders, ParentId = "4" },
-            new (){ Id = "405", Text = "Switch", Href = "/form/switch", IconName = IconName.ToggleOn, ParentId = "4" },
-            new (){ Id = "406", Text = "Time Input", Href = "/form/time-input", IconName = IconName.ClockFill, ParentId = "4" },
+                }
+            },
+            new ()
+            { 
+                Text = "Data Visualization", 
+                IconName = IconName.BarChartFill, 
+                IconColor = IconColor.Warning,
+                ChildItems = new List<Sidebar3NavItem>()
+                {
+                    new ()
+                    { 
+                        Text = "Bar Chart", 
+                        Href = "/charts/bar-chart", 
+                        IconName = IconName.BarChartFill, 
+                        Match = NavLinkMatch.All 
+                    },
+                    new ()
+                    { 
+                        Text = "Doughnut Chart", 
+                        Href = "/charts/doughnut-chart", 
+                        IconName = IconName.CircleFill,
+                        Match = NavLinkMatch.All 
+                    },
+                    new ()
+                    { 
+                        Text = "Line Chart", 
+                        Href = "/charts/line-chart", 
+                        IconName = IconName.GraphUp, 
+                        Match = NavLinkMatch.All 
+                    },
+                    new ()
+                    { 
+                        Text = "Pie Chart", 
+                        Href = "/charts/pie-chart", 
+                        IconName = IconName.PieChartFill,
+                        Match = NavLinkMatch.All 
+                    },
 
-            new (){ Id = "5", Text = "Components", IconName = IconName.GearFill, IconColor = IconColor.Danger },
-            new (){ Id = "500", Text = "Accordion", Href = "/accordion", IconName = IconName.ChevronBarExpand, ParentId = "5" },
-            new (){ Id = "501", Text = "Alerts", Href = "/alerts", IconName = IconName.CheckCircleFill, ParentId = "5" },
-            new (){ Id = "502", Text = "Badge", Href = "/badge", IconName = IconName.AppIndicator, ParentId = "5" },
-            new (){ Id = "503", Text = "Breadcrumb", Href = "/breadcrumb", IconName = IconName.SegmentedNav, ParentId = "5" },
-            new (){ Id = "504", Text = "Buttons", Href = "/buttons", IconName = IconName.ToggleOn, ParentId = "5" },
-            new (){ Id = "505", Text = "Callout", Href = "/callout", IconName = IconName.StickyFill, ParentId = "5" },
-            new (){ Id = "506", Text = "Card", Href = "/card", IconName = IconName.CardHeading, ParentId = "5" },
-            new (){ Id = "507", Text = "Charts", Href = "/charts", IconName = IconName.BarChartLineFill, ParentId = "5", Match = NavLinkMatch.All },
-            new (){ Id = "508", Text = "Collapse", Href = "/collapse", IconName = IconName.ArrowsCollapse, ParentId = "5" },
-            new (){ Id = "509", Text = "Confirm Dialog", Href = "/confirm-dialog", IconName = IconName.QuestionDiamondFill, ParentId = "5" },
-            new (){ Id = "510", Text = "Dropdown", Href = "/dropdown", IconName = IconName.MenuButtonWideFill, ParentId = "5" },
-            new (){ Id = "511", Text = "Grid", Href = "/grid", IconName = IconName.Grid, ParentId = "5" },
-            new (){ Id = "512", Text = "Modals", Href = "/modals", IconName = IconName.WindowStack, ParentId = "5" },
-            new (){ Id = "513", Text = "Offcanvas", Href = "/offcanvas", IconName = IconName.LayoutSidebarReverse, ParentId = "5" },
-            new (){ Id = "514", Text = "Pagination", Href = "/pagination", IconName = IconName.ThreeDots, ParentId = "5" },
-            new (){ Id = "515", Text = "Pdf Viewer", Href = "/pdf-viewer", IconName = IconName.FilePdfFill, ParentId = "5" },
-            new (){ Id = "516", Text = "Placeholders", Href = "/placeholders", IconName = IconName.ColumnsGap, ParentId = "5" },
-            new (){ Id = "517", Text = "Preload", Href = "/preload", IconName = IconName.ArrowClockwise, ParentId = "5" },
-            new (){ Id = "518", Text = "Progress", Href = "/progress", IconName = IconName.UsbC, ParentId = "5" },
-            new (){ Id = "519", Text = "Script Loader", Href = "/script-loader", IconName = IconName.CodeSlash, ParentId = "5" },
-            new (){ Id = "520", Text = "Sidebar", Href = "/sidebar", IconName = IconName.LayoutSidebar, ParentId = "5" },
-            new (){ Id = "521", Text = "Sidebar 2", Href = "/sidebar2", IconName = IconName.ListNested, ParentId = "5" },
-            new (){ Id = "522", Text = "Spinner", Href = "/spinners", IconName = IconName.ArrowRepeat, ParentId = "5" },
-            new (){ Id = "523", Text = "Tabs", Href = "/tabs", IconName = IconName.WindowPlus, ParentId = "5" },
-            new (){ Id = "524", Text = "Toasts", Href = "/toasts", IconName = IconName.ExclamationTriangleFill, ParentId = "5" },
-            new (){ Id = "525", Text = "Tooltips", Href = "/tooltips", IconName = IconName.ChatSquareDotsFill, ParentId = "5" },
-
-            new (){ Id = "6", Text = "Data Visualization", IconName = IconName.BarChartFill, IconColor = IconColor.Warning },
-            new (){ Id = "600", Text = "Bar Chart", Href = "/charts/bar-chart", IconName = IconName.BarChartFill, ParentId = "6", Match = NavLinkMatch.All },
-            new (){ Id = "601", Text = "Doughnut Chart", Href = "/charts/doughnut-chart", IconName = IconName.CircleFill, ParentId = "6", Match = NavLinkMatch.All },
-            new (){ Id = "602", Text = "Line Chart", Href = "/charts/line-chart", IconName = IconName.GraphUp, ParentId = "6", Match = NavLinkMatch.All },
-            new (){ Id = "603", Text = "Pie Chart", Href = "/charts/pie-chart", IconName = IconName.PieChartFill, ParentId = "6", Match = NavLinkMatch.All },
-
-            new(){ Id = "7", Text = "Services", IconName = IconName.WrenchAdjustableCircleFill, IconColor = IconColor.Success },
-            new (){ Id = "700", Text = "Modal Service", Href = "/modal-service", IconName = IconName.WindowStack, ParentId = "7" },
+                }
+            },
+            new()
+            { 
+                Text = "Services", 
+                IconName = IconName.WrenchAdjustableCircleFill, 
+                IconColor = IconColor.Success,
+                ChildItems = new List<Sidebar3NavItem>()
+                {
+                    new ()
+                    { 
+                        Text = "Modal Service", 
+                        Href = "/modal-service", 
+                        IconName = IconName.WindowStack 
+                    },
+                }
+            },
         };
-
-        return navItems;
+      
     }
 }
