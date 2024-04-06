9c3e1027af2c4e2b516b80c6f3397fd29a29f8c9
Vikram Reddy <gvreddy04@gmail.com>
2024-04-06 21:52:00 +0530

    Blazor Sortable List - Docs update

--- blazorbootstrap/Components/SortableList/SortableList.razor.cs
+++ blazorbootstrap/Components/SortableList/SortableList.razor.cs
@@ -1,13 +1,26 @@
 ﻿namespace BlazorBootstrap;
 
+/// <summary>
+/// Represents a sortable list component.
+/// </summary>
+/// <typeparam name="TItem">The type of items in the list.</typeparam>
 public partial class SortableList<TItem> : BlazorBootstrapComponentBase
 {
     #region Fields and Constants
 
+    /// <summary>
+    /// A cancellation token source for managing asynchronous operations.
+    /// </summary>
     private CancellationTokenSource cancellationTokenSource = default!;
 
+    /// <summary>
+    /// A DotNetObjectReference that allows JavaScript interop with this component.
+    /// </summary>
     private DotNetObjectReference<SortableList<TItem>>? objRef;
 
+    /// <summary>
+    /// A CSS selector used to filter disabled items.
+    /// </summary>
     private string filter = ".bb-sortable-list-item-disabled";
 
     #endregion
@@ -67,6 +80,12 @@ public partial class SortableList<TItem> : BlazorBootstrapComponentBase
     protected override bool ShouldAutoGenerateId => true;
 
     /// <summary>
+    /// Gets or sets a value indicating whether sorting is allowed for the list.
+    /// </summary>
+    [Parameter]
+    public bool AllowSorting { get; set; } = true;
+
+    /// <summary>
     /// Specifies the content to be rendered inside the <see cref="SortableList" />.
     /// </summary>
     [Parameter]
@@ -79,70 +98,94 @@ public partial class SortableList<TItem> : BlazorBootstrapComponentBase
     public List<TItem> Data { get; set; } = default!;
 
     /// <summary>
-    /// Template to render when there are no items to display.
+    /// Gets or sets a delegate that determines whether an item should be disabled.
+    /// </summary>
+    [Parameter]
+    public Func<TItem, bool> DisableItem { get; set; } = default!;
+
+    /// <summary>
+    /// Gets or sets the CSS class applied to disabled items.
+    /// </summary>
+    [Parameter]
+    public string? DisabledItemCssClass { get; set; } = default!;
+
+    /// <summary>
+    /// Specifies the template to render when there are no items to display in the list.
     /// </summary>
     [Parameter]
     public RenderFragment EmptyDataTemplate { get; set; } = default!;
 
     /// <summary>
-    /// Shows text on no records.
+    /// Gets or sets the text to display when there are no records in the list.
     /// </summary>
     [Parameter]
     public string EmptyText { get; set; } = "No records to display";
 
-    [Parameter]
-    public Func<TItem, bool> DisableItem { get; set; } = default!;
-
-    [Parameter]
-    public string? DisabledItemCssClass { get; set; } = default!;
-
     /// <summary>
-    /// Gets or sets the group.
+    /// Gets or sets the group name associated with the list.
     /// </summary>
     [Parameter]
     public string? Group { get; set; }
 
     /// <summary>
-    /// Gets or sets the drag handle selector.
+    /// Gets or sets the CSS selector for the drag handle element.
     /// </summary>
     [Parameter]
     public string? Handle { get; set; }
 
+    /// <summary>
+    /// Gets or sets a value indicating whether the list is currently loading.
+    /// </summary>
     [Parameter]
     public bool IsLoading { get; set; }
 
+    /// <summary>
+    /// Specifies the template used to render individual items in the list.
+    /// </summary>
     [Parameter]
     public RenderFragment<TItem>? ItemTemplate { get; set; }
 
     /// <summary>
-    /// Template to render when data load in-progress.
+    /// Specifies the template to render while the list data is loading.
     /// </summary>
     [Parameter]
     public RenderFragment LoadingTemplate { get; set; } = default!;
 
     /// <summary>
-    /// Gets or sets the name of the <see cref="SortableList<TItem>" />.
+    /// Gets or sets the name of the <see cref="SortableList{TItem}" /> component.
     /// </summary>
     [Parameter]
     public string? Name { get; set; }
 
+    /// <summary>
+    /// Gets or sets an event callback that fires when an item is added to the list.
+    /// </summary>
     [Parameter]
     public EventCallback<SortableListEventArgs> OnAdd { get; set; }
 
+    /// <summary>
+    /// Gets or sets an event callback that fires when an item is removed from the list.
+    /// </summary>
     [Parameter]
     public EventCallback<SortableListEventArgs> OnRemove { get; set; }
 
+    /// <summary>
+    /// Gets or sets an event callback that fires when an item is updated in the list.
+    /// </summary>
     [Parameter]
     public EventCallback<SortableListEventArgs> OnUpdate { get; set; }
 
+    /// <summary>
+    /// Gets or sets the pull mode for the sortable list.
+    /// </summary>
     [Parameter]
     public SortableListPullMode Pull { get; set; }
 
+    /// <summary>
+    /// Gets or sets the put mode for the sortable list.
+    /// </summary>
     [Parameter]
     public SortableListPutMode Put { get; set; }
-
-    [Parameter]
-    public bool AllowSorting { get; set; } = true;
 
     /// <summary>
     /// Provides JavaScript interop functionality for the Sortable List.
