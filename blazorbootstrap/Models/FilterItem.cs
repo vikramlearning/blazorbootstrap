1d234f3a79e61dfadb8ee4628761897099eb58e6
Vikram Gaddam <gvreddy04@gmail.com>
2024-05-03 16:55:45 +0530

    Grid: Filters - Enum support #485 (#703)

--- blazorbootstrap/Models/FilterItem.cs
+++ blazorbootstrap/Models/FilterItem.cs
@@ -1,3 +1,27 @@
 ﻿namespace BlazorBootstrap;
 
-public sealed record class FilterItem(string PropertyName, string Value, FilterOperator Operator, StringComparison StringComparison);
+public sealed record class FilterItem
+{
+    public FilterItem(string propertyName, string value, FilterOperator @operator, StringComparison stringComparison)
+    {
+        PropertyName = propertyName;
+        Value = value;
+        Operator = @operator;
+        StringComparison = stringComparison;
+    }
+
+    FilterItem(Type propertType, string propertyName, string value, FilterOperator @operator, StringComparison stringComparison)
+    {
+        PropertType = propertType;
+        PropertyName = propertyName;
+        Value = value;
+        Operator = @operator;
+        StringComparison = stringComparison;
+    }
+
+    public Type PropertType { get; private set; }
+    public string PropertyName { get; private set; }
+    public string Value { get; private set; }
+    public FilterOperator Operator { get; private set; }
+    public StringComparison StringComparison { get; private set; }
+}
