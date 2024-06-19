f9515c8964dbb508a41a91d97bdabd1655473cc3
Vikram Reddy <gvreddy04@gmail.com>
2024-06-19 16:54:23 +0530

    Grid Detail View updates

---
+++ blazorbootstrap/Components/Grid/GridColumns.razor.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorBootstrap;
+
+public partial class GridColumns : BlazorBootstrapComponentBase
+{
+    /// <summary>
+    /// Specifies the content to be rendered inside the grid columns component.
+    /// </summary>
+    /// <remarks>
+    /// Default value is null.
+    /// </remarks>
+    [Parameter]
+    public RenderFragment ChildContent { get; set; } = default!;
+}
