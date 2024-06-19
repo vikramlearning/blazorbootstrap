d95e75db5d27211bede99b9c6864eec05c36d92b
Vikram Reddy <gvreddy04@gmail.com>
2024-06-19 14:59:38 +0530

    Grid Detail View  - Draft

---
+++ blazorbootstrap/Components/Grid/GridDetailView.razor.cs
@@ -0,0 +1,32 @@
+﻿namespace BlazorBootstrap;
+
+public partial class GridDetailView<TItem> : BlazorBootstrapComponentBase
+{
+    private RenderFragment<TItem>? gridDetailViewTemplate;
+
+    protected override async Task OnInitializedAsync()
+    {
+        Id = IdUtility.GetNextId(); // Required
+
+        Parent.SetGridDetailView(this);
+
+        await base.OnInitializedAsync();
+    }
+
+    /// <summary>
+    /// Specifies the content to be rendered inside the grid column.
+    /// </summary>
+    /// <remarks>
+    /// Default value is null.
+    /// </remarks>
+    [Parameter]
+    public RenderFragment<TItem> ChildContent { get; set; } = default!;
+
+    internal RenderFragment<TItem> GridDetailViewTemplate =>
+        gridDetailViewTemplate ??= rowData => builder =>
+        {
+            builder.AddContent(100, ChildContent, rowData);
+        };
+
+    [CascadingParameter] public Grid<TItem> Parent { get; set; } = default!;
+}
