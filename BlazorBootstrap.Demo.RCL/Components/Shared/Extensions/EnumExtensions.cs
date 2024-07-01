8bf73bb9e0d1f26cb2520a17883f5ad17d6d8bbd
Vikram Reddy <gvreddy04@gmail.com>
2024-07-01 12:17:37 +0530

    Grid row selection - highlight the row #769 (#782)

--- BlazorBootstrap.Demo.RCL/Components/Shared/Extensions/EnumExtensions.cs
+++ BlazorBootstrap.Demo.RCL/Components/Shared/Extensions/EnumExtensions.cs
@@ -7,6 +7,8 @@ public static class EnumExtensions
         {
             LanguageCode.AspNet => "language-aspnet",
             LanguageCode.CSharp => "language-csharp",
+            LanguageCode.Css => "language-css",
+            LanguageCode.HTML => "language-html",
             LanguageCode.JavaScript => "language-js",
             LanguageCode.JSON => "language-json",
             LanguageCode.JSONP => "language-jsonp",
@@ -15,6 +17,24 @@ public static class EnumExtensions
             LanguageCode.Razor => "language-razor",
             LanguageCode.Text => "language-none",
             LanguageCode.YAML => "language-yaml",
+            _ => null
+        };
+
+    public static string? ToLanguageCodeString(this LanguageCode languageCode) =>
+        languageCode switch
+        {
+            LanguageCode.AspNet => "ASP.NET",
+            LanguageCode.CSharp => "C#",
+            LanguageCode.Css => "CSS",
+            LanguageCode.HTML => "HTML",
+            LanguageCode.JavaScript => "JS",
+            LanguageCode.JSON => "JSON",
+            LanguageCode.JSONP => "JSONP",
+            LanguageCode.Markdown => "Markdown",
+            LanguageCode.PowerShell => "PowerShell",
+            LanguageCode.Razor => "Razor",
+            LanguageCode.Text => "Text",
+            LanguageCode.YAML => "yaml",
             _ => null
         };
 }
