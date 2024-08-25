d3ce81fdca0dfdf994bacc063689e3b7eef8c73a
Vikram Reddy <gvreddy04@gmail.com>
2024-08-25 16:48:39 +0530

    Carousel component (#850)
    * New Carousel component

---
+++ blazorbootstrap/Components/Carousel/CarouselItem.razor.cs
@@ -0,0 +1,62 @@
+﻿namespace BlazorBootstrap;
+
+public partial class CarouselItem : BlazorBootstrapComponentBase
+{
+    #region Methods
+
+    protected override async Task OnInitializedAsync()
+    {
+        Id = IdUtility.GetNextId(); // Required
+        Parent.AddItem(this);
+        await base.OnInitializedAsync();
+    }
+
+    #endregion
+
+    #region Properties, Indexers
+
+    protected override string? ClassNames =>
+        BuildClassNames(
+            Class,
+            (BootstrapClass.CarouselItem, true),
+            (BootstrapClass.Active, Active)
+        );
+
+    /// <summary>
+    /// Gets or sets the active state.
+    /// </summary>
+    /// <remarks>
+    /// Default value is <see langword="false" />.
+    /// </remarks>
+    [Parameter]
+    public bool Active { get; set; }
+
+    /// <summary>
+    /// Gets or sets the content to be rendered within the component.
+    /// </summary>
+    /// <remarks>
+    /// Default value is <see langword="null" />.
+    /// </remarks>
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    /// <summary>
+    /// The amount of time to delay between automatically cycling an item.
+    /// </summary>
+    /// <remarks>
+    /// Default value is 5000.
+    /// </remarks>
+    [Parameter]
+    public int? Interval { get; set; } = 5000;
+
+    /// <summary>
+    /// Gets or sets the aria-label.
+    /// </summary>
+    [Parameter]
+    public string? Label { get; set; }
+
+    [CascadingParameter(Name = "Carousel")] 
+    public Carousel Parent { get; set; } = default!;
+
+    #endregion
+}
---
+++ blazorbootstrap/EventArguments/CarouselEventArgs.cs
@@ -0,0 +1,19 @@
+﻿namespace BlazorBootstrap;
+
+public class CarouselEventArgs : EventArgs
+{
+    /// <summary>
+    /// The direction in which the <see cref="Carousel" /> is sliding (either "left" or "right").
+    /// </summary>
+    public string? Direction { get; set; }
+
+    /// <summary>
+    /// The index of the current item.
+    /// </summary>
+    public int From { get; set; }
+
+    /// <summary>
+    /// The index of the next item.
+    /// </summary>
+    public int To { get; set; }
+}
