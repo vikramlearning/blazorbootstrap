2276dc6f3e1b63d3df1362ecb6224284661326f8
Vikram Reddy <gvreddy04@gmail.com>
2024-09-07 12:05:00 +0530

    ChartDataset - code cleanup

--- blazorbootstrap/Models/Charts/ChartDataset/ChartDataset.cs
+++ blazorbootstrap/Models/Charts/ChartDataset/ChartDataset.cs
@@ -3,82 +3,82 @@
 public interface IChartDataset { }
 
 /// <summary>
-/// <See href="https://www.chartjs.org/docs/latest/general/data-structures.html#dataset-configuration" />
+///     <See href="https://www.chartjs.org/docs/latest/general/data-structures.html#dataset-configuration" />
 /// </summary>
 public class ChartDataset<TData> : IChartDataset
 {
-  #region Constructors
+    #region Constructors
 
-  public ChartDataset()
-  {
-    Oid = Guid.NewGuid();
-  }
+    public ChartDataset()
+    {
+        Oid = Guid.NewGuid();
+    }
 
-  #endregion
+    #endregion
 
-  #region Properties, Indexers
+    #region Properties, Indexers
 
-  /// <summary>
-  /// How to clip relative to chartArea. Positive value allows overflow, negative value clips that many pixels inside
-  /// chartArea. 0 = clip at chartArea.
-  /// Clipping can also be configured per side: clip: {left: 5, top: false, right: -2, bottom: 0}
-  /// </summary>
-  /// <remarks>
-  /// Default value is <see langword="null"/>.
-  /// </remarks>
-  [JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
-  public string? Clip { get; set; }
+    /// <summary>
+    /// How to clip relative to chartArea. Positive value allows overflow, negative value clips that many pixels inside
+    /// chartArea. 0 = clip at chartArea.
+    /// Clipping can also be configured per side: clip: {left: 5, top: false, right: -2, bottom: 0}
+    /// </summary>
+    /// <remarks>
+    /// Default value is <see langword="null" />.
+    /// </remarks>
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public string? Clip { get; set; }
 
-  /// <summary>
-  /// Get or sets the Data.
-  /// </summary>
-  /// <remarks>
-  /// Default value is <see langword="null"/>.
-  /// </remarks>
-  [JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
-  public List<TData>? Data { get; set; }
+    /// <summary>
+    /// Get or sets the Data.
+    /// </summary>
+    /// <remarks>
+    /// Default value is <see langword="null" />.
+    /// </remarks>
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public List<TData>? Data { get; set; }
 
-  /// <summary>
-  /// Configures the visibility state of the dataset. Set it to <see langword="true"/>, to hide the dataset from the chart.
-  /// </summary>
-  /// <remarks>
-  /// Default value is <see langword="false"/>.
-  /// </remarks>
-  public bool Hidden { get; set; }
+    /// <summary>
+    /// Configures the visibility state of the dataset. Set it to <see langword="true" />, to hide the dataset from the chart.
+    /// </summary>
+    /// <remarks>
+    /// Default value is <see langword="false" />.
+    /// </remarks>
+    public bool Hidden { get; set; }
 
-  /// <summary>
-  /// The label for the dataset which appears in the legend and tooltips.
-  /// </summary>
-  /// <remarks>
-  /// Default value is <see cref="string.Empty" />.
-  /// </remarks>
-  [JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
-  public string? Label { get; set; }
+    /// <summary>
+    /// The label for the dataset which appears in the legend and tooltips.
+    /// </summary>
+    /// <remarks>
+    /// Default value is <see cref="string.Empty" />.
+    /// </remarks>
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public string? Label { get; set; }
 
-  /// <summary>
-  /// Get unique object id.
-  /// </summary>
-  public Guid Oid { get; private set; }
+    /// <summary>
+    /// Get unique object id.
+    /// </summary>
+    public Guid Oid { get; private set; }
 
-  /// <summary>
-  /// The drawing order of dataset. Also affects order for stacking, tooltip and legend.
-  /// </summary>
-  /// <remarks>
-  /// Default value is 0.
-  /// </remarks>
-  public int Order { get; set; }
+    /// <summary>
+    /// The drawing order of dataset. Also affects order for stacking, tooltip and legend.
+    /// </summary>
+    /// <remarks>
+    /// Default value is 0.
+    /// </remarks>
+    public int Order { get; set; }
 
-  //Stack
-  //https://www.chartjs.org/docs/latest/general/data-structures.html#dataset-configuration
+    //Stack
+    //https://www.chartjs.org/docs/latest/general/data-structures.html#dataset-configuration
 
-  /// <summary>
-  /// Gets or sets the chart type.
-  /// </summary>
-  /// <remarks>
-  /// Default value is <see langword="null"/>.
-  /// </remarks>
-  [JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
-  public string? Type { get; protected set; }
+    /// <summary>
+    /// Gets or sets the chart type.
+    /// </summary>
+    /// <remarks>
+    /// Default value is <see langword="null" />.
+    /// </remarks>
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public string? Type { get; protected set; }
 
-  #endregion
+    #endregion
 }
