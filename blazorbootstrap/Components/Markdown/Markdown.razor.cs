752010d332b1ff5215c2d655ec411df5e909ac04
Vikram Reddy <gvreddy04@gmail.com>
2024-09-14 08:01:45 +0530

    Markdown component setup inprogress

---
+++ blazorbootstrap/Components/Markdown/Markdown.razor.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorBootstrap;
+
+public partial class Markdown : BlazorBootstrapComponentBase
+{
+    #region Properties, Indexers
+
+    /// <summary>
+    /// Gets or sets the content to be rendered within the component.
+    /// </summary>
+    /// <remarks>
+    /// Default value is null.
+    /// </remarks>
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    #endregion
+}
