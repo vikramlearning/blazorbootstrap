a686b107d73cd694ddb6cb8ad90edba334a59cec
Vikram Reddy <gvreddy04@gmail.com>
2024-10-26 22:27:29 +0530

    BlazorBootstrapLayout - updates

--- blazorbootstrap/Components/Layout/BlazorBootstrapLayout.razor.cs
+++ blazorbootstrap/Components/Layout/BlazorBootstrapLayout.razor.cs
@@ -8,17 +8,14 @@ public partial class BlazorBootstrapLayout : BlazorBootstrapLayoutComponentBase
 
     [Parameter] public RenderFragment? ContentSection { get; set; }
     [Parameter] public string? ContentSectionCssClass { get; set; }
-    protected string? ContentSectionCssClassNames => BuildClassNames(ContentSectionCssClass, ("px-4 py-2", true));
+    protected string? ContentSectionCssClassNames => BuildClassNames(ContentSectionCssClass, ("p-4", true));
 
     [Parameter] public RenderFragment? FooterSection { get; set; }
     [Parameter] public string? FooterSectionCssClass { get; set; }
-    protected string? FooterSectionCssClassNames => BuildClassNames(ContentSectionCssClass, ("bb-footer p-4 mt-4", true));
-
-
+    protected string? FooterSectionCssClassNames => BuildClassNames(FooterSectionCssClass, ("bb-footer p-4", true));
+    
     [Parameter] public RenderFragment? HeaderSection { get; set; }
-
     [Parameter] public string? HeaderSectionCssClass { get; set; }
-
     protected string? HeaderSectionCssClassNames => BuildClassNames(HeaderSectionCssClass, ("bb-top-row px-4", true));
 
     [Parameter] public RenderFragment? SidebarSection { get; set; }
