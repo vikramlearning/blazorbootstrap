181347df85a99884b1fb04c7087795e94668af13
Vikram Reddy <gvreddy04@gmail.com>
2024-10-26 14:57:41 +0530

    BlazorBootstrap Layout - updates

---
+++ blazorbootstrap/Components/Layout/BlazorBootstrapLayout.razor.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorBootstrap;
+
+public partial class BlazorBootstrapLayout : LayoutComponentBase
+{
+    [Parameter]
+    public RenderFragment? SidebarSection { get; set; }
+
+    [Parameter]
+    public RenderFragment? HeaderSection { get; set; }
+
+    [Parameter]
+    public RenderFragment? ContentSection { get; set; }
+
+    [Parameter]
+    public RenderFragment? FooterSection { get; set; }
+}
