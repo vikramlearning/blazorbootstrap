909a65427d570ba1241e8e063ef8a17eac0fc8f9
Vikram Reddy <gvreddy04@gmail.com>
2024-11-03 22:25:39 +0530

    RCL - Shared components updated

---
+++ BlazorBootstrap.Demo.RCL/Components/Shared/PageMetaTags.razor.cs
@@ -0,0 +1,39 @@
+﻿namespace BlazorBootstrap.Demo.RCL;
+
+public partial class PageMetaTags : BlazorBootstrapComponentBase
+{
+    #region Properties, Indexers
+
+    /// <summary>
+    /// Meta Description.
+    /// </summary>
+    [Parameter]
+    public string Description { get; set; } = null!;
+
+    /// <summary>
+    /// Meta Image Url.
+    /// </summary>
+    [Parameter]
+    public string ImageUrl { get; set; } = null!;
+
+    /// <summary>
+    /// Meta Url.
+    /// Example: /alerts, /modal
+    /// </summary>
+    [Parameter]
+    public string PageUrl { get; set; } = null!;
+
+    private string siteName => "Blazor Bootstrap";
+
+    private string title => $"{Title} | {siteName}";
+
+    /// <summary>
+    /// Page Title / Meta Title.
+    /// </summary>
+    [Parameter]
+    public string Title { get; set; } = null!;
+
+    private string url => $"https://demos.blazorbootstrap.com{PageUrl}";
+
+    #endregion
+}
---
+++ BlazorBootstrap.Demo.RCL/Constants/RouteConstants.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazorBootstrap.Demo.RCL;
+
+public static class RouteConstants
+{
+    public const string Blog_Prefix = "/blog";
+    public const string Docs_Prefix = "/docs";
+    public const string Demos_Prefix = "/demos";
+
+    #region Demos
+
+    // Features
+    public const string Demos_Accordion_Documentation = Demos_Prefix + "/accordion";
+
+    #endregion
+}
