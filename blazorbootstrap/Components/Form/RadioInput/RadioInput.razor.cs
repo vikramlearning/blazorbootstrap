34726d95e76d58a0119426f67454ba8c95a5b6c9
Vikram Reddy <gvreddy04@gmail.com>
2024-12-13 23:05:36 +0530

    Form components (#978)
    * New Form Components (CheckboxInput, RadioInput, TextInput, TextAreaInput)
    * Removed IConfiguration dependency

---
+++ blazorbootstrap/Components/Form/RadioInput/RadioInput.razor.cs
@@ -0,0 +1,97 @@
+﻿namespace BlazorBootstrap;
+
+public partial class RadioInput : BlazorBootstrapComponentBase
+{
+    #region Fields and Constants
+
+    private FieldIdentifier fieldIdentifier;
+
+    #endregion
+
+    #region Methods
+
+    protected override void OnInitialized()
+    {
+        AdditionalAttributes ??= new Dictionary<string, object>();
+
+        fieldIdentifier = FieldIdentifier.Create(ValueExpression);
+
+        base.OnInitialized();
+    }
+
+    /// <summary>
+    /// Disables number input.
+    /// </summary>
+    public void Disable() => Disabled = true;
+
+    /// <summary>
+    /// Enables number input.
+    /// </summary>
+    public void Enable() => Disabled = false;
+
+    private async Task OnChange(ChangeEventArgs e)
+    {
+        var oldValue = Value;
+        var newValue = string.Equals(e.Value?.ToString(), "on");
+
+        await ValueChanged.InvokeAsync(newValue);
+
+        Console.WriteLine($"Old value: {oldValue}, New value: {e.Value}");
+
+        EditContext?.NotifyFieldChanged(fieldIdentifier);
+    }
+
+    #endregion
+
+    #region Properties, Indexers
+
+    protected override string? ClassNames =>
+        BuildClassNames(
+            Class,
+            (BootstrapClass.FormCheckInput, true),
+            (EditContext?.FieldCssClass(fieldIdentifier) ?? string.Empty, true)
+        );
+
+    /// <summary>
+    /// Gets or sets the disabled state.
+    /// </summary>
+    /// <remarks>
+    /// Default value is false.
+    /// </remarks>
+    [Parameter]
+    public bool Disabled { get; set; }
+
+    /// <summary>
+    /// Gets the associated <see cref="Microsoft.AspNetCore.Components.Forms.EditContext" />.
+    /// </summary>
+    [CascadingParameter]
+    private EditContext EditContext { get; set; } = default!;
+
+    /// <summary>
+    /// Gets or sets the label.
+    /// </summary>
+    [Parameter]
+    public string? Label { get; set; }
+
+    /// <summary>
+    /// Gets or sets the name.
+    /// </summary>
+    [Parameter]
+    public string? Name { get; set; }
+
+    /// <summary>
+    /// Gets or sets the value.
+    /// </summary>
+    [Parameter]
+    public bool Value { get; set; } = default!;
+
+    /// <summary>
+    /// This event fired on every user keystroke that changes the NumberInput value.
+    /// </summary>
+    [Parameter]
+    public EventCallback<bool> ValueChanged { get; set; }
+
+    [Parameter] public Expression<Func<bool>> ValueExpression { get; set; } = default!;
+
+    #endregion
+}
