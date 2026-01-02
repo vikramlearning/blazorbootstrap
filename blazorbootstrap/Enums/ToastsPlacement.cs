28f82b84ce4af9ce5f8f551955f28ba1af4fe6d2
Vikram Reddy <gvreddy04@gmail.com>
2026-01-02 11:14:50 +0530

    Enhance Toasts docs and add API metadata attributes (#1230)
    Improved XML documentation and added [AddedVersion], [DefaultValue], and [Description] attributes to Toasts components, enums, and related classes. Updated documentation pages to include enum details. These changes improve API discoverability and support for documentation tooling.

--- blazorbootstrap/Enums/ToastsPlacement.cs
+++ blazorbootstrap/Enums/ToastsPlacement.cs
@@ -7,40 +7,56 @@ public enum ToastsPlacement
     /// <summary>
     /// Top center
     /// </summary>
+    [AddedVersion("1.0.0")]
+    [Description("Top center")]
     TopCenter,
 
     /// <summary>
     /// Top right
     /// </summary>
+    [AddedVersion("1.0.0")]
+    [Description("Top right")]
     TopRight,
 
     /// <summary>
     /// Middle left
     /// </summary>
+    [AddedVersion("1.0.0")]
+    [Description("Middle left")]
     MiddleLeft,
 
     /// <summary>
     /// Middle center
     /// </summary>
+    [AddedVersion("1.0.0")]
+    [Description("Middle center")]
     MiddleCenter,
 
     /// <summary>
     /// Middle right
     /// </summary>
+    [AddedVersion("1.0.0")]
+    [Description("Middle right")]
     MiddleRight,
 
     /// <summary>
     /// Bottom left
     /// </summary>
+    [AddedVersion("1.0.0")]
+    [Description("Bottom left")]
     BottomLeft,
 
     /// <summary>
     /// Bottom center
     /// </summary>
+    [AddedVersion("1.0.0")]
+    [Description("Bottom center")]
     BottomCenter,
 
     /// <summary>
     /// Bottom right
     /// </summary>
+    [AddedVersion("1.0.0")]
+    [Description("Bottom right")]
     BottomRight
 }
